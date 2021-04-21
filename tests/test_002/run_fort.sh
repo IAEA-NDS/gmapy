b4b3c7a3ffbfd272745565b0bff062eafd0a2229
gschnabel <40870991+gschnabel@users.noreply.github.com>
2021-04-21 17:50:25 +0200

    update GMAP-Fortran commit id in test_002

--- tests/test_002/run_fort.sh
+++ tests/test_002/run_fort.sh
@@ -6,7 +6,7 @@
 
 basedir=`pwd`
 GMAP_fortran_url="https://github.com/IAEA-NDS/GMAP-Fortran.git"
-GMAP_fortran_commit_id="a774a96da48697a93e9e5ce0c440b0de2b3f0b14"
+GMAP_fortran_commit_id="a99ef97bff517aa9c1ebd3bb47cae2157ab538de"
 GMAP_python_dir="$basedir/../../source"
 GMAP_python_exe="$GMAP_python_dir/GMAP.py"
 
