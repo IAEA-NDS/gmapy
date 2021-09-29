d3df5b3fbd69998b0ff414724f93342ba598654e
gschnabel <40870991+gschnabel@users.noreply.github.com>
2021-09-29 17:51:20 +0200

    refactor: move should_downweight to new source file

--- source/gmap_functions.py
+++ source/gmap_functions.py
@@ -1,6 +1,7 @@
 from generic_utils import unflatten, Bunch
 from fortran_utils import fort_range, fort_read, fort_write
 from data_management import init_datablock
+from gmap_snippets import should_downweight
 import numpy as np
 
 import linpack_slim
@@ -186,21 +187,6 @@ def deal_with_dataset(MC1, MC2, MC3, MC4, MC5, MC6, MC7, MC8,
         fill_AA_AM_COV(data, fisdata, APR, gauss, file_IO4)
 
     return
-
-def should_downweight(ID, data):
-    MOD2 = data.MOD2
-    AMO3 = data.AMO3
-    IDEN = data.IDEN
-    # 1st if line: downweighting based on year of measurement
-    # 2nd if line: downweighting of specified data sets
-    # 3rd+ if line: downweighting data sets with tags .NE. 1
-    if (MOD2 > 1000 and IDEN[ID, 3] < MOD2) or \
-       (MOD2 == 10 and IDEN[ID, 6] in NRED[1:(NELI+1)]) or \
-       (MOD2 == 1 and IDEN[ID, 4] != 1) or \
-       (MOD2 < 0 and IDEN[ID, 4] != 1):
-        return True
-    else:
-        return False
 
 
 def write_dataset_info(ID, data, LABL, file_IO4):
