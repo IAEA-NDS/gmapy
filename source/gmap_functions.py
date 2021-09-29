b509585395c4a64df413991f537ec4c9aca17764
gschnabel <40870991+gschnabel@users.noreply.github.com>
2021-09-29 17:44:41 +0200

    refactor: move write_dataset_info to new source file

--- source/gmap_functions.py
+++ source/gmap_functions.py
@@ -2,6 +2,7 @@ from generic_utils import unflatten, Bunch
 from fortran_utils import fort_range, fort_read, fort_write
 from data_management import init_datablock
 from gmap_snippets import should_downweight
+from output_management import write_dataset_info
 import numpy as np
 
 import linpack_slim
@@ -188,41 +189,6 @@ def deal_with_dataset(MC1, MC2, MC3, MC4, MC5, MC6, MC7, MC8,
 
     return
 
-
-def write_dataset_info(ID, data, LABL, file_IO4):
-
-    IDEN = data.IDEN
-    NCT = data.NCT[ID]
-    NT = data.NT[ID,:]
-    NS = IDEN[ID,6]
-    MT = IDEN[ID,7]
-
-    format142 = "(//, ' ***********DATASET**************************** '/)"
-    fort_write(file_IO4, format142, [])
-    NU = NCT
-    if NCT > 4:
-        NU = 4
-    NCT2 = NCT - NU
-    NU1 = NU + 1
-    format139 = "(2X,8HDATA SET,I5,2X,A16,4(2X,2A8))"
-    tmp = [[LABL.CLAB[NT[K],L] for L in fort_range(1,2)] for K in fort_range(1,NU)]
-    fort_write(file_IO4, format139, [NS, LABL.TYPE[MT],tmp])
-    if NCT2 > 0:
-        format149 = "(2X,6(2X,2A8))"
-        tmp = [[LABL.CLAB[NT[K],L] for L in fort_range(1,2)] for K in fort_range(NU1,NCT2)]
-        fort_write(file_IO4, format149, tmp)
-
-    #
-    #       NAME ID AND REFERENCE I/O
-    #
-    format132 = "(/' YEAR',I5,' TAG',I3,' AUTHOR:  ',4A8,4A8/)"
-    fort_write(None, format132, [IDEN[ID, 3:5], LABL.CLABL[1:5], LABL.BREF[1:5]])
-    # VP      if(modrep .ne. 0) go to 183
-    fort_write(file_IO4, format132, [IDEN[ID, 3:5], LABL.CLABL[1:5], LABL.BREF[1:5]])
-
-    if not should_downweight(ID, data) and (data.MOD2 > 1 and data.MOD2 < 10):
-        format339 = "('  WEIGHTING OPTION NOT IMPLEMENTED, DATA SET  ',I5/)"
-        fort_write(file_IO4, format339, NS)
 
 
 def read_dataset_input(MC1, MC2, MC3, MC4, MC5, MC6, MC7, MC8,
