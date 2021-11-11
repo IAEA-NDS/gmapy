8b922920dc0d1ea605f1f1cb873492308973ee01
gschnabel <40870991+gschnabel@users.noreply.github.com>
2021-11-11 15:57:25 +0100

    refactor: remove unused arg fisdata from several functions

--- source/gmap_functions.py
+++ source/gmap_functions.py
@@ -74,7 +74,7 @@ def read_prior(MC1, MC2, APR, file_IO3):
 
 
 
-def read_datablock(APR, fisdata, MODC, MOD2, AMO3,
+def read_datablock(APR, MODC, MOD2, AMO3,
         IELIM, NELIM, MPPP, MODREP, LABL, file_IO3):
 
     format100 = "(A4,1X,8I5)"
@@ -93,8 +93,7 @@ def read_datablock(APR, fisdata, MODC, MOD2, AMO3,
         # LABL.AKON[2] == 'DATA'
         if ACON == LABL.AKON[2]:
             deal_with_dataset(MC1, MC2, MC3, MC4, MC5, MC6, MC7, MC8,
-                    data, fisdata,
-                    APR, IELIM, NELIM,
+                    data, APR, IELIM, NELIM,
                     MPPP, MODREP, file_IO3)
 
         # LABL.AKON[7] == 'EDBL'
@@ -133,8 +132,7 @@ def prepare_for_datablock_input(MODC, MOD2, AMO3):
 
 
 def deal_with_dataset(MC1, MC2, MC3, MC4, MC5, MC6, MC7, MC8,
-        data, fisdata,
-        APR, IELIM, NELIM,
+        data, APR, IELIM, NELIM,
         MPPP, MODREP, file_IO3):
 
     read_dataset_input(
