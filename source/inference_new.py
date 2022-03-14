688a5ac2d209bc53f530eed6ae9444fb16f13369
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-03-14 17:49:21 +0100

    refactor: implement create_priortable for new prior structure

--- source/inference_new.py
+++ source/inference_new.py
@@ -4,6 +4,60 @@ from scipy.sparse.linalg import spsolve
 from scipy.linalg.lapack import dpotri, dpotrf
 
 from mappings.compound_map import CompoundMap
+from collections import OrderedDict
+
+
+
+def create_priortable(prior_list):
+    curid = 0
+    df = []
+    for item in prior_list:
+        if item['type'] == 'legacy-prior-cross-section':
+            curid += 1
+            xsid = item['ID']
+            # error checking
+            if curid != xsid:
+                raise IndexError('prior items must be sorted according to ID in prior_list ' +
+                        'but prior item with ID %d violates this constraint' % xsid)
+            if not np.all(np.sort(item['EN']) == item['EN']):
+                raise ValueError('Energies of prior mesh must be sorted, but this is ' +
+                        'not the case for prior block with ID %d' % xsid)
+            if not len(item['EN']) == len(item['CS']):
+                raise IndexError('Energy mesh and cross sections are of unequal length ' +
+                        'for prior block with ID %d' % xsid)
+
+            # append to the dataframe
+            prd = prior_dic = OrderedDict()
+            prd['NODE'] = 'xsid_' + str(xsid)
+            prd['REAC'] = 'MT:1-R1:' + str(xsid)
+            prd['ENERGY'] = item['EN']
+            prd['PRIOR'] = item['CS']
+            prd['DESCR'] = item['CLAB'].strip()
+            curdf = pd.DataFrame.from_dict(prd)
+            df.append(curdf)
+
+        elif item['type'] == 'legacy-fission-spectrum':
+            # error checking
+            if not np.all(np.sort(item['ENFIS']) == item['ENFIS']):
+                raise ValueError('Energies of prior mesh of fission spectrum must be sorted ' +
+                        'but this is not the case for the the legacy-fission-spectrum')
+            if not len(item['ENFIS']) == len(item['FIS']):
+                raise IndexError('Energy mesh and fission spectrum values must be of same length')
+            # append to the dataframe
+            prd = prior_dic = OrderedDict()
+            prd['NODE'] = 'fis'
+            prd['REAC'] = 'NA'
+            prd['ENERGY'] = item['ENFIS']
+            prd['PRIOR'] = item['FIS']
+            prd['DESCR'] = 'fission spectrum'
+            curdf = pd.DataFrame.from_dict(prd)
+            df.append(curdf)
+
+        else:
+            raise ValueError('Unknown type "%s" of prior block' % item['type'])
+
+    df = pd.concat(df)
+    return df
 
 
 
