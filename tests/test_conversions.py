851a17a41371debf241bcbc4a9b6336824232c0c
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-03-24 13:23:36 +0100

    add first unittest TestCase for db conversion

---
+++ tests/test_conversions.py
@@ -0,0 +1,33 @@
+import unittest
+import pathlib
+
+from gmapi.legacy.database_reading import read_gma_database
+from gmapi.legacy.conversion_utils import (sanitize_datablock,
+        desanitize_datablock, compare_legacy_datablock_lists,
+        augment_datablocks_with_NTOT)
+
+
+class TestConversions(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls._dbpath = (pathlib.Path(__file__).parent / 'testdata' / 'data-2017-08-14.gma').resolve().as_posix()
+        cls._dbdic = read_gma_database(cls._dbpath)
+
+    @classmethod
+    def tearDownClass(cls):
+        del(cls._dbpath)
+        del(cls._dbdic)
+
+    def test_datablock_conversion(self):
+        dbdic = self._dbdic
+        blocklist = dbdic['datablock_list'] 
+        new_blocklist = [sanitize_datablock(b) for b in blocklist]
+        old_blocklist = [desanitize_datablock(b) for b in new_blocklist]
+        augment_datablocks_with_NTOT(old_blocklist)
+        cmpval = compare_legacy_datablock_lists(blocklist, old_blocklist)
+        self.assertTrue(cmpval)
+
+
+if __name__ == '__main__':
+    unittest.main()
