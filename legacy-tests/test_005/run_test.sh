3ce878f7972df3294160792a31e4dd700970bff0
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-03-27 22:54:33 +0200

    add legacy test_005 without PPP correction

---
+++ legacy-tests/test_005/run_test.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+
+# DESCRIPTION
+#   compare if the gfortran and Python version
+#   yield the same result
+
+basedir=`pwd`
+GMAP_fortran_url="https://github.com/IAEA-NDS/GMAP-Fortran.git"
+GMAP_fortran_commit_id="1dce067fdeb6a0c5d11b799269df011ae136edbd"
+GMAP_python_dir="$basedir/../../"
+GMAP_python_exe="$GMAP_python_dir/examples/example-001-run-gmap.py"
+
+if [ ! -d "GMAP-Fortran" ]; then
+    git clone $GMAP_fortran_url
+    retcode=$?
+    if [ $retcode -ne "0" ]; then
+        print "Downloading Fortran version of GMAP from $GMAP_fortran_url failed"
+        exit $retcode
+    fi
+    if [ ! -d "GMAP-Fortran "]; then
+        print "The directory expected to contain the Fortran source code does not exist"
+        exit $?
+    fi
+    cd GMAP-Fortran \
+    && git checkout $GMAP_fortran_commit_id \
+    && rm -rf .git \
+    && cd source \
+    && gfortran -o GMAP GMAP.FOR \
+    && mv GMAP .. \
+    && chmod +x GMAP
+    cd "$basedir"
+fi
+
+if [ ! -d result/fortran ]; then
+    mkdir -p result/fortran
+    cp input/data.gma result/fortran/
+    cd result/fortran/
+    ../../GMAP-Fortran/GMAP
+    # Fortran print -0.0 whereas Python 0.0
+    # thus remove the odd minus sign from Fortran output
+    # two times each sed comment to deal with the case
+    # -0.00-0.00 and to make sure both minuses are removed
+    sed -i -e 's/-\(00*\.0*\)\([^0-9]\|$\)/ \1\2/g' gma.res
+    sed -i -e 's/-\(00*\.0*\)\([^0-9]\|$\)/ \1\2/g' gma.res
+    sed -i -e 's/-\(00*\.0*\)\([^0-9]\|$\)/ \1\2/g' plot.dta
+    sed -i -e 's/-\(00*\.0*\)\([^0-9]\|$\)/ \1\2/g' plot.dta
+    sed -i -e 's/-\(\.00*\)\([^0-9]\|$\)/0\1\2/g' gma.res
+    sed -i -e 's/-\(\.00*\)\([^0-9]\|$\)/0\1\2/g' gma.res
+    # introduce a substitution because Python and Fortran
+    # produce in two cases a different terminal digit
+    # due to the fact that in the Pyton version we update
+    # a variable by multiplication with AZ before printing
+    # and then during printing reverse that effect by
+    # dividing by AZ.
+    sed -i -e '69344,69344s/0\.7790E-02/0.7789E-02/' gma.res
+    sed -i -e '35981,35981s/0\.7790E-02/0.7789E-02/' gma.res
+    sed -i -e '35994,35994s/0\.7790E-02/0.7789E-02/' gma.res
+    sed -i -e '97592,97592s/1\.18 /1.17 /' gma.res
+    sed -i -e '102694,102694s/0\.7790E-02/0.7789E-02/' gma.res
+    cd "$basedir"
+fi
+
+# generate Python result
+mkdir -p result/python
+cp input/gmadata.json result/python/
+cd result/python/
+PYTHONPATH="$GMAP_python_dir"
+python -c '
+import pandas as pd
+from gmapi.gmap import run_gmap;
+res = run_gmap(dbfile="gmadata.json", dbtype="json", correct_ppp=False)
+res["table"].to_csv("restable.csv", sep=";")
+'
+
+# ignore insignificant small change in python result
+sed -i -e '69344,69344s/0\.7790E-02/0.7789E-02/' gma.res
+sed -i -e '35981,35981s/0\.7790E-02/0.7789E-02/' gma.res
+sed -i -e '35994,35994s/0\.7790E-02/0.7789E-02/' gma.res
+sed -i -e '97592,97592s/1\.18 /1.17 /' gma.res
+sed -i -e '102694,102694s/0\.7790E-02/0.7789E-02/' gma.res
+# when using the json database we get two more instances
+# of a difference by 1 in the terminal digit which isn't
+# a problem.
+sed -i -e '35981,35981s/0\.7790E-02/0.7789E-02/' gma.res
+sed -i -e '97592,97592s/1\.18/1.17/' gma.res
+
+# compare the results
+cd "$basedir"
+diff result/fortran/gma.res result/python/gma.res \
+  && diff result/fortran/plot.dta result/python/plot.dta
+
+retcode=$?
+exit $retcode
