ae07be5deb5d9ad3e312f579810d2702e8e8714f
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-03-28 09:11:29 +0200

    add test GMAP identical result on json database

--- tests/test_conversions.py
+++ tests/test_conversions.py
@@ -1,11 +1,14 @@
 import unittest
 import pathlib
+from tempfile import TemporaryDirectory
+import numpy as np
 
+from gmapi.gmap import run_gmap
 from gmapi.legacy.database_reading import read_gma_database
 from gmapi.legacy.conversion_utils import (sanitize_datablock,
         desanitize_datablock, compare_legacy_datablock_lists,
         augment_datablocks_with_NTOT, sanitize_prior,
-        desanitize_prior)
+        desanitize_prior, convert_GMA_database_to_JSON)
 
 
 class TestConversions(unittest.TestCase):
@@ -19,6 +22,22 @@ class TestConversions(unittest.TestCase):
     def tearDownClass(cls):
         del(cls._dbpath)
         del(cls._dbdic)
+
+    def test_result_equivalence_for_converted_database(self):
+        dbpath = self._dbpath
+        with TemporaryDirectory() as d:
+            dirpath = str(d)
+            jsonpath = (pathlib.Path(dirpath) / 'gmadata.json').resolve().as_posix()
+            convert_GMA_database_to_JSON(dbpath, jsonpath)
+            upd_res1 = run_gmap(dbpath, dbtype='legacy',
+                    num_iter=1, legacy_output=False)
+            upd_res2 = run_gmap(jsonpath, dbtype='json',
+                    num_iter=1, legacy_output=False)
+            res1 = upd_res1['table']['POST'].to_numpy()
+            res2 = upd_res2['table']['POST'].to_numpy()
+            relerr = np.abs(res1-res2) / np.maximum(1e-8, res2)
+            maxrelerr = np.max(relerr)
+            self.assertLess(maxrelerr, 1e-8)
 
     def test_datablock_conversion(self):
         dbdic = self._dbdic
