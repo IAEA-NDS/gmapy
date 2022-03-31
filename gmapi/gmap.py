efc19115d21256ce54f3fcdb7d9c256c872d6215
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-03-31 17:44:28 +0200

    set default of legacy_output to True

--- gmapi/gmap.py
+++ gmapi/gmap.py
@@ -28,7 +28,7 @@ from .mappings.compound_map import CompoundMap
 
 
 def run_gmap(dbfile='data.gma', resfile='gma.res', plotfile='plot.dta',
-        dbtype='legacy', num_iter=3, correct_ppp=True, legacy_output=True,
+        dbtype='legacy', num_iter=3, correct_ppp=True, legacy_output=False,
         fix_ppp_bug=True, fix_sacs_jacobian=True, format_dic={}):
 
     # BEGIN LEGACY
