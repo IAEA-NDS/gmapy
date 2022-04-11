096ca36937950634694fe709aa60f07c4cd3ce3f
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-04-11 17:49:44 +0200

    add test with permuted input for get_basic_integral_of_products_sensmats

--- tests/test_basic_integral_of_product_mappings.py
+++ tests/test_basic_integral_of_product_mappings.py
@@ -108,6 +108,45 @@ class TestBasicIntegralOfProductJacobian(unittest.TestCase):
         self.assertTrue(np.all(np.isclose(test_res[1], ref_res[1], rtol=1e-5)))
         self.assertTrue(np.all(np.isclose(test_res[2], ref_res[2], rtol=1e-5)))
 
+    def test_basic_integral_of_product_sensmats_with_permuted_input(self):
+        np.random.seed(17)
+        x1 = np.array([1, 3, 8, 14])
+        y1 = np.array([7, 2, 9, 16])
+        perm1 = np.random.permutation(len(x1))
+        perm_x1 = x1[perm1]
+        perm_y1 = y1[perm1]
+        interp1 = 'lin-lin'
+        x2 = np.array([4, 5.5, 6.9, 8.3])
+        y2 = np.array([11, 22, 3, 29])
+        perm2 = np.random.permutation(len(x2))
+        perm_x2 = x2[perm2]
+        perm_y2 = y2[perm2]
+        interp2 = 'log-log'
+        x3 = np.array([2.1, 4.9, 7.8])
+        y3 = np.array([5,   5.5, 2.4])
+        interp3 = 'lin-log'
+        perm3 = np.random.permutation(len(x3))
+        perm_x3 = x3[perm3]
+        perm_y3 = y3[perm3]
+
+        xlist = [x1, x2, x3]
+        ylist = [y1, y2, y3]
+        interplist = [interp1, interp2, interp3]
+        perm_xlist = [perm_x3, perm_x1, perm_x2]
+        perm_ylist = [perm_y3, perm_y1, perm_y2]
+        perm_interplist = [interp3, interp1, interp2]
+
+        test_res1 = get_basic_integral_of_product_sensmats(xlist, ylist, interplist,
+                zero_outside=True, maxord=10, rtol=1e-3)
+        test_res2 = get_basic_integral_of_product_sensmats(perm_xlist, perm_ylist,
+                perm_interplist, zero_outside=True, maxord=10, rtol=1e-3)
+
+        test_res1 = [np.ravel(t) for t in test_res1]
+        test_res2 = [np.ravel(t) for t in test_res2]
+
+        self.assertTrue(np.all(test_res1[0][perm1] == test_res2[1]))
+        self.assertTrue(np.all(test_res1[1][perm2] == test_res2[2]))
+        self.assertTrue(np.all(test_res1[2][perm3] == test_res2[0]))
 
 if __name__ == '__main__':
     unittest.main()
