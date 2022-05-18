7d5de91271636efa6fd779e3bd5f169edb1cd470
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-05-18 12:04:49 +0200

    implement run_gmap_simplified

--- gmapi/gmap.py
+++ gmapi/gmap.py
@@ -28,7 +28,6 @@ from .mappings.compound_map import CompoundMap
 #   START OF GMAP PROGRAM
 ##################################################
 
-
 def run_gmap(dbfile='data.gma', resfile='gma.res', plotfile='plot.dta',
         dbtype='legacy', num_iter=3, correct_ppp=True, legacy_output=False,
         fix_ppp_bug=True, fix_sacs_jacobian=True, legacy_integration=True,
@@ -189,6 +188,117 @@ def run_gmap(dbfile='data.gma', resfile='gma.res', plotfile='plot.dta',
         file_IO4.close()
         file_IO5.close()
     # END LEGACY
+
+    datatable['PRIOR'] = orig_priorvals
+
+    postvals = datatable['PRIOR'].to_numpy()
+    postvals[prior_idcs] = upd_vals
+    datatable['POST'] = compmap.propagate(datatable, postvals)
+    datatable['POSTUNC'] = np.full(len(datatable), np.NaN, dtype=float)
+    datatable.loc[prior_idcs, 'POSTUNC'] = np.sqrt(np.diag(upd_covmat))
+    datatable['RELPOSTUNC'] = datatable['POSTUNC'].to_numpy() / datatable['POST'].to_numpy()
+    return {'table': datatable, 'postcov': upd_covmat, 'idcs': prior_idcs,
+            'priorcov': covmat}
+
+
+def read_legacy_gma_database(dbfile):
+    format_dic = {}
+    db_dic = read_gma_database(dbfile, format_dic=format_dic)
+    legacy_datablock_list = db_dic['datablock_list']
+    prior_list = sanitize_prior(db_dic['APR'])
+    datablock_list = [sanitize_datablock(b) for b in legacy_datablock_list]
+    return {'prior_list': prior_list, 'datablock_list': datablock_list}
+
+
+def read_json_gma_database(dbfile):
+    with open(dbfile, 'r') as f:
+        db_dic = json.load(f)
+    return {'prior_list': db_dic['prior'],
+            'datablock_list': db_dic['datablocks']}
+
+
+def run_gmap_simplified(prior_list=None, datablock_list=None,
+        dbfile=None, dbtype='legacy', num_iter=3, correct_ppp=True):
+
+    compmap = CompoundMap(fix_sacs_jacobian=True,
+                          legacy_integration=False)
+
+    if dbfile is not None:
+        if dbtype == 'legacy':
+            db_dic = read_legacy_gma_database(dbfile)
+        elif dbtype == 'json':
+            db_dic = read_json_gma_database(dbfile)
+        else:
+            raise ValueError('dbtype must be "legacy" or "json"')
+
+        if prior_list is None:
+            prior_list = db_dic['prior_list']
+
+        if datablock_list is None:
+            datablock_list = db_dic['datablock_list']
+
+    if prior_list is None:
+        raise TypeError('you must provide prior_list or a dbfile')
+    if datablock_list is None:
+        raise TypeError('you must provide datablock_list or a dbfile')
+
+    priortable = create_prior_table(prior_list)
+    exptable = create_experiment_table(datablock_list)
+
+    datatable = pd.concat([priortable, exptable], axis=0, ignore_index=True)
+    refvals = datatable['PRIOR'].to_numpy()
+
+    uncs = np.full(len(refvals), np.nan)
+    expsel = datatable['NODE'].str.match('exp_').to_numpy()
+    uncs[expsel] = create_relunc_vector(datablock_list)
+    datatable = attach_shape_prior(datatable, compmap, refvals, uncs)
+
+    MODREP = 0
+    expsel = datatable['NODE'].str.match('exp_').to_numpy()
+    exp_idcs = datatable.index[expsel].to_numpy()
+    nonexp_idcs = datatable.index[np.logical_not(expsel)]
+    uncs = np.full(len(datatable), 0.)
+    uncs[exp_idcs] = create_relunc_vector(datablock_list)
+
+    orig_priorvals = datatable['PRIOR'].to_numpy().copy()
+    while True:
+
+        refvals = datatable['PRIOR'].to_numpy()
+        propvals = compmap.propagate(datatable, refvals)
+        update_dummy_datapoints(datatable, propvals)
+        if correct_ppp:
+            effuncs = calculate_PPP_correction(datatable, compmap, refvals, uncs)
+        else:
+            effuncs = uncs.copy()
+
+        expdata = datatable['DATA'].to_numpy()
+        # construct covariance matrix
+        uncs_red = uncs[expsel]
+        effuncs_red = effuncs[expsel]
+        expdata_red = expdata[expsel]
+        tmp = create_experimental_covmat(datablock_list, expdata_red, uncs_red,
+                                         effuncs_red)
+        tmp = coo_matrix(tmp)
+        covmat = csr_matrix((tmp.data, (exp_idcs[tmp.row], exp_idcs[tmp.col])),
+                               shape=(len(datatable), len(datatable)),
+                               dtype=float)
+        del(tmp)
+        # add prior uncertainties
+        prioruncs = datatable.loc[nonexp_idcs, 'UNC'].to_numpy()
+        covmat += csr_matrix((np.square(prioruncs), (nonexp_idcs, nonexp_idcs)),
+                             shape=(len(datatable), len(datatable)), dtype=float)
+
+        upd_res = gls_update(compmap, datatable, covmat, retcov=True)
+        prior_idcs = upd_res['idcs']
+        upd_vals = upd_res['upd_vals']
+        upd_covmat = upd_res['upd_covmat']
+
+        datatable.loc[prior_idcs, 'PRIOR'] = upd_vals
+
+        if (num_iter == 0 or MODREP == num_iter):
+            break
+
+        MODREP=MODREP+1
 
     datatable['PRIOR'] = orig_priorvals
 
