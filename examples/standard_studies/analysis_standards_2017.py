f7bf25912965b5f808851ca3852ce49e65c7c0f8
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-06-15 14:07:55 +0200

    add analysis scripts to investigate SACS measurements
    Analysis scripts were added to study the comparatively
    low value of PU9(n,f) SACS resulting from a GMA evaluation
    with the updated database file compared to the value obtained
    by using the same SACS and ratio of SACS measurements adopted
    by Mannhart in his evaluation.

---
+++ examples/standard_studies/analysis_standards_2017.py
@@ -0,0 +1,40 @@
+import sys
+sys.path.append('..')
+sys.path.append('../..')
+from copy import deepcopy
+from gmapi.data_management.datablock_list import DatablockList
+from gmapi.gmap import run_gmap_simplified
+from gmapi.data_management.database_IO import (read_json_gma_database,
+        read_legacy_gma_database)
+
+from convenience_funs import (get_sacs_predictions,
+        remove_absolute_datasets, output_sacs_preds)
+
+# read the database with NIFFTE TPC data
+dbpath = '../../legacy-tests/test_002/input/data.gma'
+db_dic = read_legacy_gma_database(dbpath)
+prior_list = db_dic['prior_list']
+datablock_list = db_dic['datablock_list']
+
+gls_result = run_gmap_simplified(prior_list=prior_list, datablock_list=datablock_list)
+ref_table = gls_result['table']
+ref_sacs = get_sacs_predictions(ref_table, gls_result['postcov'])
+
+# systematically remove a group of absolute cross sections
+# 8: U5(n,f), 9: PU9(n,f), 10: U8(n,f)
+exclude_reacids = (8,9,10) 
+res_list = []
+for cur_reacid in exclude_reacids:
+    reduced_dblist = remove_absolute_datasets(datablock_list, cur_reacid)
+    gls_result = run_gmap_simplified(prior_list=prior_list, datablock_list=reduced_dblist)
+    curtable = gls_result['table']
+    curcov = gls_result['postcov']
+    cur_sacs = get_sacs_predictions(curtable, curcov)
+    cur_sacs.update({'removed_reacid': cur_reacid})
+    res_list.append(cur_sacs)
+
+
+output_sacs_preds('output/results_05.txt', ref_sacs, res_list, comment="""
+############################################
+standards data 2017
+""")
