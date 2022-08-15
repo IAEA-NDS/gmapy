2b4512c4022951bab309d9e8b0e43d80863af6a9
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-08-15 14:39:54 +0200

    extend test cases for run_GMAP_simplified

--- tests/test_gmap_simplified.py
+++ tests/test_gmap_simplified.py
@@ -14,7 +14,7 @@ class TestGmapSimplified(unittest.TestCase):
         # the simplified one without the possibility to reproduce
         # the bugs of the legacy version
         res2 = run_gmap_simplified(dbfile=dbpath, dbtype='legacy',
-                num_iter=3, correct_ppp=True)
+                num_iter=3, correct_ppp=True, remove_dummy=False)
         # the version with more comprehensive input options
         res1 = run_gmap(dbfile=dbpath, dbtype='legacy', legacy_output=False,
                 num_iter=3, correct_ppp=True,
@@ -33,6 +33,58 @@ class TestGmapSimplified(unittest.TestCase):
         self.assertTrue(np.all(p1.indices == p2.indices))
         self.assertTrue(np.all(p1.indptr == p2.indptr))
         self.assertTrue(np.allclose(p1.data, p2.data, equal_nan=True))
+
+    def test_GMAP_simplified_difference_if_no_dummy_data(self):
+        dbpath = (pathlib.Path(__file__).parent / 'testdata' /
+                'data-2017-07-26.gma').resolve().as_posix()
+        res2 = run_gmap_simplified(dbfile=dbpath, dbtype='legacy',
+                num_iter=3, correct_ppp=True, remove_dummy=True)
+        res1 = run_gmap(dbfile=dbpath, dbtype='legacy', legacy_output=False,
+                num_iter=3, correct_ppp=True,
+                fix_ppp_bug=True, fix_sacs_jacobian=True, legacy_integration=False)
+        # extract the results
+        xs1 = res1['table']['POST'][res1['table'].NODE.str.match('xsid_')].to_numpy()
+        xs2 = res2['table']['POST'][res2['table'].NODE.str.match('xsid_')].to_numpy()
+        absdiff = np.abs(xs1 - xs2)
+        reldiff = absdiff / (1e-8 + np.abs(xs2))
+        maxreldiff = np.max(reldiff)
+        print(maxreldiff)
+        self.assertTrue(np.all(np.isclose(xs1, xs2, rtol=1e-4)))
+
+    def test_GMAP_simplified_difference_if_more_iterations(self):
+        dbpath = (pathlib.Path(__file__).parent / 'testdata' /
+                'data-2017-07-26.gma').resolve().as_posix()
+        res2 = run_gmap_simplified(dbfile=dbpath, dbtype='legacy',
+                num_iter=8, correct_ppp=True, remove_dummy=False)
+        res1 = run_gmap(dbfile=dbpath, dbtype='legacy', legacy_output=False,
+                num_iter=3, correct_ppp=True,
+                fix_ppp_bug=True, fix_sacs_jacobian=True, legacy_integration=False)
+        # extract the results
+        xs1 = res1['table']['POST'][res1['table'].NODE.str.match('xsid_')].to_numpy()
+        xs2 = res2['table']['POST'][res2['table'].NODE.str.match('xsid_')].to_numpy()
+        absdiff = np.abs(xs1 - xs2)
+        reldiff = absdiff / (1e-8 + np.abs(xs2))
+        maxreldiff = np.max(reldiff)
+        print(maxreldiff)
+        self.assertTrue(np.all(np.isclose(xs1, xs2, rtol=1e-4)))
+
+    def test_GMAP_simplified_difference_if_no_dummydata_and_more_iterations(self):
+        dbpath = (pathlib.Path(__file__).parent / 'testdata' /
+                'data-2017-07-26.gma').resolve().as_posix()
+        res2 = run_gmap_simplified(dbfile=dbpath, dbtype='legacy',
+                num_iter=8, correct_ppp=True, remove_dummy=True)
+        res1 = run_gmap(dbfile=dbpath, dbtype='legacy', legacy_output=False,
+                num_iter=3, correct_ppp=True,
+                fix_ppp_bug=True, fix_sacs_jacobian=True, legacy_integration=False)
+        # extract the results
+        xs1 = res1['table']['POST'][res1['table'].NODE.str.match('xsid_')].to_numpy()
+        xs2 = res2['table']['POST'][res2['table'].NODE.str.match('xsid_')].to_numpy()
+        absdiff = np.abs(xs1 - xs2)
+        reldiff = absdiff / (1e-8 + np.abs(xs2))
+        maxreldiff = np.max(reldiff)
+        print(maxreldiff)
+        self.assertTrue(np.all(np.isclose(xs1, xs2, rtol=1e-4)))
+
 
 
 if __name__ == '__main__':
