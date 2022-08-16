c8825498d172ac22613eb96a0823cca28782e258
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-08-16 11:06:37 +0200

    fix: take properly into account prior covmat in lm_update

--- gmapi/inference.py
+++ gmapi/inference.py
@@ -123,7 +123,8 @@ def lm_update(mapping, datatable, covmat, retcov=False, startvals=None,
 
         # GLS update
         inv_post_cov = S.T @ obscovmat_fact(S) + inv_prior_cov + lmb * idmat
-        postvals = refvals + spsolve(inv_post_cov, S.T @ (spsolve(obscovmat, meas-preds)))
+        zvec = S.T @ spsolve(obscovmat, meas-preds) + priorcovmat_fact(priorvals-refvals)
+        postvals = refvals + spsolve(inv_post_cov, zvec)
 
         # calculate real prediction and expected prediction
         # according to linearization for posterior parameters
