0c334b51c67769e9d019c554c807e603cfdb83a5
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-08-19 10:09:23 +0200

    introduce convenience function to read database file

--- gmapi/data_management/database_IO.py
+++ gmapi/data_management/database_IO.py
@@ -1,10 +1,10 @@
 import json
-from ..legacy.database_reading import read_gma_database
+from ..legacy.database_reading import read_gma_database as read_legacy_gma_database_basic
 from ..legacy.conversion_utils import sanitize_datablock, sanitize_prior
 
 def read_legacy_gma_database(dbfile):
     format_dic = {}
-    db_dic = read_gma_database(dbfile, format_dic=format_dic)
+    db_dic = read_legacy_gma_database_basic(dbfile, format_dic=format_dic)
     legacy_datablock_list = db_dic['datablock_list']
     prior_list = sanitize_prior(db_dic['APR'])
     datablock_list = [sanitize_datablock(b) for b in legacy_datablock_list]
@@ -16,4 +16,15 @@ def read_json_gma_database(dbfile):
         db_dic = json.load(f)
     return {'prior_list': db_dic['prior'],
             'datablock_list': db_dic['datablocks']}
+
+
+def read_gma_database(dbfile):
+    with open(dbfile, 'r') as f:
+        firstline = f.readline()
+    is_json = firstline.strip().startswith('{')
+    if is_json:
+        db = read_json_gma_database(dbfile)
+    else:
+        db = read_legacy_gma_database(dbfile)
+    return db
 
