def820a0021dca86092ab699d1f22b84bc0cf305
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-02-03 22:35:18 +0100

    rewrite CrossSectionTotalMap to use autodiff

--- gmapy/mappings/cross_section_total_map.py
+++ gmapy/mappings/cross_section_total_map.py
@@ -1,8 +1,11 @@
 import numpy as np
-from .basic_maps import (basic_propagate, get_basic_sensmat,
-        basic_multiply_Sdic_rows)
-from .helperfuns import return_matrix
-
+from .mapping_elements import (
+    Selector,
+    SelectorCollection,
+    Distributor,
+    LinearInterpolation
+)
+from .helperfuns import return_matrix_new
 
 
 class CrossSectionTotalMap:
@@ -12,28 +15,14 @@ class CrossSectionTotalMap:
                    datatable['NODE'].str.match('exp_', na=False))
         return np.array(expmask, dtype=bool)
 
-
     def propagate(self, datatable, refvals):
-        propdic = self.__compute(datatable, refvals, 'propagate')
-        propvals = np.full(datatable.shape[0], 0.)
-        propvals[propdic['idcs2']] = propdic['propvals']
-        return propvals
-
+        return self.__compute(datatable, refvals, 'propagate')
 
     def jacobian(self, datatable, refvals, ret_mat=False):
-        jac = self.__compute(datatable, refvals, 'jacobian')
-        return return_matrix(jac['idcs1'], jac['idcs2'], jac['coeffs'],
-                  dims = (datatable.shape[0], datatable.shape[0]),
-                  how = 'csr' if ret_mat else 'dic')
-
+        S = self.__compute(datatable, refvals, 'jacobian')
+        return return_matrix_new(S, how='csr' if ret_mat else 'dic')
 
     def __compute(self, datatable, refvals, what):
-        idcs1 = np.empty(0, dtype=int)
-        idcs2 = np.empty(0, dtype=int)
-        coeff = np.empty(0, dtype=float)
-        propvals = np.empty(0, dtype=float)
-        concat = np.concatenate
-
         priormask = (datatable['REAC'].str.match('MT:1-R1:', na=False) &
                      datatable['NODE'].str.match('xsid_', na=False))
         priortable = datatable[priormask]
@@ -41,10 +30,12 @@ class CrossSectionTotalMap:
         exptable = datatable[expmask]
         reacs = exptable['REAC'].unique()
 
+        inpvars = []
+        outvars = []
         for curreac in reacs:
             # obtian the involved reactions
             reac_groups = curreac.split('-')[1:]
-            reacids = [int(x.split(':')[1]) for x in reac_groups] 
+            reacids = [int(x.split(':')[1]) for x in reac_groups]
             reacstrs = ['MT:1-R1:' + str(rid) for rid in reacids]
             if len(np.unique(reacstrs)) < len(reacstrs):
                    raise IndexError('Each reaction must occur only once in reaction string')
@@ -55,44 +46,26 @@ class CrossSectionTotalMap:
             # some abbreviations
             src_idcs_list = [pt.index for pt in priortable_reds]
             src_en_list = [pt['ENERGY'] for pt in priortable_reds]
-            src_vals_list = [refvals[pt.index] for pt in priortable_reds]
             tar_idcs = exptable_red.index
             tar_en = exptable_red['ENERGY']
-            # calculate the sensitivity matrix
-            # d(a1+a2+a3+...) = 1*da1 + 1*da2 + 1*da3 + ...
-            curvals = np.full(len(tar_idcs), 0., dtype=float)
-            for i in range(len(src_idcs_list)):
-                cur_src_en = src_en_list[i]
-                cur_src_vals = src_vals_list[i]
-                curvals += basic_propagate(cur_src_en, cur_src_vals, tar_en)
 
-            if what == 'jacobian':
-                Sdic = {'idcs1': np.empty(0, dtype=int),
-                        'idcs2': np.empty(0, dtype=int),
-                        'x': np.empty(0, dtype=float)}
-                for i in range(len(src_en_list)):
-                    cur_src_idcs = src_idcs_list[i]
-                    cur_src_en = src_en_list[i]
-                    cur_src_vals = src_vals_list[i]
-                    curSdic = get_basic_sensmat(cur_src_en, cur_src_vals,
-                                                tar_en, ret_mat=False)
-                    curSdic['idcs1'] = cur_src_idcs[curSdic['idcs1']]
-                    curSdic['idcs2'] = tar_idcs[curSdic['idcs2']]
+            cvars = [Selector(idcs, len(datatable)) for idcs in src_idcs_list]
+            inpvars.extend(cvars)
+            cvars_int = []
+            for cv, en in zip(cvars, src_en_list):
+                cvars_int.append(LinearInterpolation(cv, en, tar_en))
 
-                    Sdic['idcs1'] = concat([Sdic['idcs1'], curSdic['idcs1']])
-                    Sdic['idcs2'] = concat([Sdic['idcs2'], curSdic['idcs2']])
-                    Sdic['x'] = concat([Sdic['x'], curSdic['x']])
+            tmpres = sum(cvars_int)
+            outvar = Distributor(tmpres, tar_idcs, len(datatable))
+            outvars.append(outvar)
 
-                idcs1 = concat([idcs1, Sdic['idcs1']])
-                idcs2 = concat([idcs2, Sdic['idcs2']])
-                coeff = concat([coeff, Sdic['x']])
+        inp = SelectorCollection(inpvars)
+        out = sum(outvars)
+        inp.assign(refvals)
 
-            elif what == 'propagate':
-                idcs2 = concat([idcs2, tar_idcs])
-                propvals = concat([propvals, curvals])
-
-        if what == 'jacobian':
-            return {'idcs1': idcs1, 'idcs2': idcs2, 'coeffs': coeff}
-        elif what == 'propagate':
-            return {'idcs2': idcs2, 'propvals': propvals}
-
+        if what == 'propagate':
+            return out.evaluate()
+        elif what == 'jacobian':
+            return out.jacobian()
+        else:
+            raise ValueError(f'what "{what}" not implemented"')
