25c0674a7d70729c41d1d2ddbd05e8a88e893dfa
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-02-03 22:07:11 +0100

    rewrite CrossSectionShapeOfRatioMap to use autodiff

--- gmapy/mappings/cross_section_shape_of_ratio_map.py
+++ gmapy/mappings/cross_section_shape_of_ratio_map.py
@@ -1,8 +1,12 @@
 import numpy as np
-from .basic_maps import (basic_propagate, get_basic_sensmat,
-        basic_multiply_Sdic_rows)
-from .helperfuns import return_matrix
-
+from .mapping_elements import (
+    Selector,
+    SelectorCollection,
+    Replicator,
+    Distributor,
+    LinearInterpolation
+)
+from .helperfuns import return_matrix_new
 
 
 class CrossSectionShapeOfRatioMap:
@@ -12,28 +16,14 @@ class CrossSectionShapeOfRatioMap:
                    datatable['NODE'].str.match('exp_', na=False))
         return np.array(expmask, dtype=bool)
 
-
     def propagate(self, datatable, refvals):
-        propdic = self.__compute(datatable, refvals, 'propagate')
-        propvals = np.full(datatable.shape[0], 0.)
-        propvals[propdic['idcs2']] = propdic['propvals']
-        return propvals
-
+        return self.__compute(datatable, refvals, 'propagate')
 
     def jacobian(self, datatable, refvals, ret_mat=False):
-        jac = self.__compute(datatable, refvals, 'jacobian')
-        return return_matrix(jac['idcs1'], jac['idcs2'], jac['coeffs'],
-                  dims = (datatable.shape[0], datatable.shape[0]),
-                  how = 'csr' if ret_mat else 'dic')
-
+        S = self.__compute(datatable, refvals, 'jacobian')
+        return return_matrix_new(S, how='csr' if ret_mat else 'dic')
 
     def __compute(self, datatable, refvals, what):
-        idcs1 = np.empty(0, dtype=int)
-        idcs2 = np.empty(0, dtype=int)
-        coeff = np.empty(0, dtype=float)
-        propvals = np.empty(0, dtype=float)
-        concat = np.concatenate
-
         priormask = (datatable['REAC'].str.match('MT:1-R1:', na=False) &
                      datatable['NODE'].str.match('xsid_', na=False))
         priormask = np.logical_or(priormask, datatable['NODE'].str.match('norm_', na=False))
@@ -42,6 +32,8 @@ class CrossSectionShapeOfRatioMap:
         exptable = datatable[expmask]
         reacs = exptable['REAC'].unique()
 
+        inpvars = []
+        outvars = []
         for curreac in reacs:
             # obtain the involved reactions
             string_groups = curreac.split('-')
@@ -66,9 +58,11 @@ class CrossSectionShapeOfRatioMap:
             src_en1 = priortable_red1['ENERGY']
             src_en2 = priortable_red2['ENERGY']
             src_en3 = priortable_red3['ENERGY']
-            src_vals1 = refvals[priortable_red1.index]
-            src_vals2 = refvals[priortable_red2.index]
-            src_vals3 = refvals[priortable_red3.index]
+
+            inpvar1 = Selector(src_idcs1, len(datatable))
+            inpvar2 = Selector(src_idcs2, len(datatable))
+            inpvar3 = Selector(src_idcs3, len(datatable))
+            inpvars.extend([inpvar1, inpvar2, inpvar3])
 
             exptable_red = exptable[exptable['REAC'].str.fullmatch(curreac, na=False)]
             datasets = exptable_red['NODE'].unique()
@@ -81,52 +75,25 @@ class CrossSectionShapeOfRatioMap:
                 norm_index = priortable[priortable['NODE'].str.fullmatch(normstr, na=False)].index
                 if len(norm_index) != 1:
                     raise IndexError('Exactly one normalization factor must be present for a dataset')
-                norm_fact = refvals[norm_index]
-                # calculate the sensitivity matrix
-                # d(a/(b+c)) = 1/(b+c)*da - a/(b+c)^2*db - a/(b+c)^2*dc
-                propvals1 = basic_propagate(src_en1, src_vals1, tar_en)
-                propvals2 = basic_propagate(src_en2, src_vals2, tar_en)
-                propvals3 = basic_propagate(src_en3, src_vals3, tar_en)
 
-                if what == 'jacobian':
-                    Sdic1 = get_basic_sensmat(src_en1, src_vals1, tar_en, ret_mat=False)
-                    Sdic1['idcs1'] = src_idcs1[Sdic1['idcs1']]
-                    Sdic1['idcs2'] = tar_idcs[Sdic1['idcs2']]
+                norm_fact = Selector(norm_index, len(datatable))
+                inpvars.append(norm_fact)
+                norm_fact_rep = Replicator(norm_fact, len(tar_idcs))
 
-                    Sdic2 = get_basic_sensmat(src_en2, src_vals2, tar_en, ret_mat=False)
-                    Sdic2['idcs1'] = src_idcs2[Sdic2['idcs1']]
-                    Sdic2['idcs2'] = tar_idcs[Sdic2['idcs2']]
+                inpvar1_int = LinearInterpolation(inpvar1, src_en1, tar_en)
+                inpvar2_int = LinearInterpolation(inpvar2, src_en2, tar_en)
+                inpvar3_int = LinearInterpolation(inpvar3, src_en3, tar_en)
+                tmpres = norm_fact_rep * inpvar1_int / (inpvar2_int + inpvar3_int)
+                outvar = Distributor(tmpres, tar_idcs, len(datatable))
+                outvars.append(outvar)
 
-                    Sdic3 = get_basic_sensmat(src_en3, src_vals3, tar_en, ret_mat=False)
-                    Sdic3['idcs1'] = src_idcs3[Sdic3['idcs1']]
-                    Sdic3['idcs2'] = tar_idcs[Sdic3['idcs2']]
+        inp = SelectorCollection(inpvars)
+        out = sum(outvars)
+        inp.assign(refvals)
 
-                    basic_multiply_Sdic_rows(Sdic1, 1 / (propvals2+propvals3) * norm_fact)
-                    basic_multiply_Sdic_rows(Sdic2, (-propvals1 / np.square(propvals2+propvals3)) * norm_fact)
-                    basic_multiply_Sdic_rows(Sdic3, (-propvals1 / np.square(propvals2+propvals3)) * norm_fact)
-
-                    Sdic = {'idcs1': concat([Sdic1['idcs1'], Sdic2['idcs1'], Sdic3['idcs1']]),
-                            'idcs2': concat([Sdic1['idcs2'], Sdic2['idcs2'], Sdic3['idcs2']]),
-                            'x': concat([Sdic1['x'], Sdic2['x'], Sdic3['x']])}
-                    # add the normalization sensitivity
-                    src_norm_idcs = np.full(len(tar_idcs), norm_index)
-                    norm_coeffs = propvals1 / (propvals2+propvals3)
-                    Sdic['idcs1'] = concat([Sdic['idcs1'], src_norm_idcs])
-                    Sdic['idcs2'] = concat([Sdic['idcs2'], tar_idcs])
-                    Sdic['x'] = concat([Sdic['x'], norm_coeffs])
-
-                    # add everything to global triple list
-                    idcs1 = concat([idcs1, Sdic['idcs1']])
-                    idcs2 = concat([idcs2, Sdic['idcs2']])
-                    coeff = concat([coeff, Sdic['x']])
-
-                elif what == 'propagate':
-                    idcs2 = concat([idcs2, tar_idcs])
-                    curvals = propvals1 / (propvals2+propvals3) * norm_fact
-                    propvals = concat([propvals, curvals])
-
-        if what == 'jacobian':
-            return {'idcs1': idcs1, 'idcs2': idcs2, 'coeffs': coeff}
-        elif what == 'propagate':
-            return {'idcs2': idcs2, 'propvals': propvals}
-
+        if what == 'propagate':
+            return out.evaluate()
+        elif what == 'jacobian':
+            return out.jacobian()
+        else:
+            raise ValueError(f'what "{what}" not implemented"')
