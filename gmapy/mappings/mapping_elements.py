d0677945f8c5aad7e9a6138dcb8d25dfc4e2ad6b
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-02-05 22:00:13 +0100

    implement IntegralOfProduct for autodiff

--- gmapy/mappings/mapping_elements.py
+++ gmapy/mappings/mapping_elements.py
@@ -1,6 +1,10 @@
 import numpy as np
 from scipy.sparse import csr_matrix, vstack
 from .basic_maps import get_basic_sensmat
+from .basic_integral_maps import (
+    basic_integral_of_product_propagate,
+    get_basic_integral_of_product_sensmats
+)
 from .helperfuns import numeric_jacobian
 
 
@@ -224,43 +228,44 @@ class LinearInterpolation(MyAlgebra):
         return self.__jacobian @ self.__obj.jacobian()
 
 
-if __name__ == "__main__":
+class IntegralOfProduct(MyAlgebra):
 
-    def test_jacobian(x0, obj, *inpobjs):
-        for inpobj in inpobjs:
-            inpobj.assign(x0)
-        return obj.jacobian().toarray()
+    def __init__(self, obj_list, xlist, interplist,
+                 zero_outside=False, **kwargs):
+        if not all(isinstance(obj, MyAlgebra) for obj in obj_list):
+            raise TypeError('all objects in obj_list must be of type ' +
+                            'obj_list')
+        if len(obj_list) != len(xlist):
+            raise IndexError('length of obj_list must equal length ' +
+                             'of xlist')
+        if len(interplist) != len(obj_list):
+            raise IndexError('length of interp_list must equal ' +
+                             'length of obj_list')
+        self.__obj_list = obj_list
+        self.__xlist = [np.array(xv) for xv in xlist]
+        self.__interplist = interplist
+        self.__zero_outside = zero_outside
+        self.__kwargs = kwargs
 
-    def reference_jacobian(x0, obj, *inpobjs):
-        def eval(x):
-            for inpobj in inpobjs:
-                inpobj.assign(x)
-            return obj.evaluate()
-        return numeric_jacobian(eval, x0)
+    def __len__(self):
+        return 1
 
-    def is_jacobian_correct(x0, obj, *inpobjs):
-        test_jac = test_jacobian(x0, obj, *inpobjs)
-        ref_jac = reference_jacobian(x0, obj, *inpobjs)
-        return np.allclose(test_jac, ref_jac)
+    def evaluate(self):
+        ylist = [obj.evaluate() for obj in self.__obj_list]
+        return basic_integral_of_product_propagate(
+            self.__xlist, ylist, self.__interplist,
+            self.__zero_outside, **self.__kwargs
+        )
 
-    inpvec = np.array([1, 2, 3, 4, 5, 6])
-    # case 1
-    x = Selector([0, 1, 2], 6)
-    y = Selector([3, 4, 5], 6)
-    z1 = LinearInterpolation(x, [1, 2, 3], [1.5, 2, 2.5])
-    z2 = LinearInterpolation(y, [1, 2, 3], [1, 2, 3])
-    z = z1 + z2
-    is_jacobian_correct(inpvec, z, x, y)
-    # case 2
-    c = Const([1.]*3)
-    z = (x+c)/y
-    is_jacobian_correct(inpvec, z, x, y)
-    # case 3
-    x1 = Selector([0, 1, 2], 6)
-    x2 = Selector([1, 2, 3], 6)
-    x3 = Selector([2, 3, 4], 6)
-    z = x1 + x2 + x3
-    is_jacobian_correct(inpvec, z, x1, x2, x3)
-    # case 4
-    z = x1 * x2 * x3
-    is_jacobian_correct(inpvec, z, x1, x2, x3)
+    def jacobian(self):
+        ylist = [obj.evaluate() for obj in self.__obj_list]
+        outer_jacs = get_basic_integral_of_product_sensmats(
+            self.__xlist, ylist, self.__interplist,
+            self.__zero_outside, **self.__kwargs
+        )
+        outer_jacs = [csr_matrix(mat) for mat in outer_jacs]
+        inner_jacs = [obj.jacobian() for obj in self.__obj_list]
+        jac = 0.
+        for outer_jac, inner_jac in zip(outer_jacs, inner_jacs):
+            jac += outer_jac @ inner_jac
+        return jac
