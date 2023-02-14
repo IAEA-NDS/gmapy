56ec2e1e51d8b80771f8a334f7e4327cc2190d70
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-02-14 19:29:22 +0100

    introduce check_norm arg and in legacy_maps.py and minor tidying up

--- gmapy/legacy/legacy_maps.py
+++ gmapy/legacy/legacy_maps.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 
-
-def propagate_fisavg(ens, vals, ensfis, valsfis):
+def propagate_fisavg(ens, vals, ensfis, valsfis, check_norm=True):
     ord = np.argsort(ens)
     ens = ens[ord]
     vals = vals[ord]
@@ -14,7 +13,6 @@ def propagate_fisavg(ens, vals, ensfis, valsfis):
     # as lowest energy 2.53e-8 MeV
     fidx = np.searchsorted(ensfis[1:], ens[1]) + 1
 
-    lentmp = len(ensfis)
     ensfis = np.concatenate([ensfis, np.full(100, 0.)])
     valsfis = np.concatenate([valsfis, np.full(100, 0.)])
 
@@ -44,12 +42,11 @@ def propagate_fisavg(ens, vals, ensfis, valsfis):
     sfl = sfl + valsfis[0]*vals[0] + valsfis[urealidx-1]*vals[-1]
     sfis = sfl / fl
 
-    if not np.isclose(1., fl, atol=0, rtol=1e-4):
+    if check_norm and not np.isclose(1., fl, atol=0, rtol=1e-4):
         print('fission normalization: ' + str(fl))
         raise ValueError('fission spectrum not normalized')
 
     return sfis
-
 
 
 def get_sensmat_fisavg(ens, vals, ensfis, valsfis):
@@ -65,23 +62,19 @@ def get_sensmat_fisavg(ens, vals, ensfis, valsfis):
     # as lowest energy 2.53e-8 MeV
     fidx = np.searchsorted(ensfis[1:], ens[1]) + 1
 
-    uhypidx = fidx+len(ens)-1
-    urealidx = min(fidx+len(ens)-1, len(ensfis))
-    ulimidx2 = len(ens) - (uhypidx - urealidx)
     # TODO: This check fails for the 9Pu(n,f) cross section fission average
     #       because the energy 0.235 MeV is missing in 9Pu(n,f) but present
     #       in the fission spectrum
     # if not np.all(np.isclose(ens[1:ulimidx2], ensfis[fidx:urealidx], atol=0, rtol=1e-05)):
     #     raise ValueError('energies of excitation function and fission spectrum do not match')
 
-    lentmp = len(ensfis)
     ensfis = np.concatenate([ensfis, np.full(100, 0.)])
     valsfis = np.concatenate([valsfis, np.full(100, 0.)])
 
     sensvec = np.full(len(ens), 0., dtype=float)
 
     fl = 0.
-    for i in range(0, len(ens)): 
+    for i in range(0, len(ens)):
         fl = fl + valsfis[fidx+i]
         if i == 0 or i == len(ens)-1:
             cssj = vals[i]
@@ -100,8 +93,7 @@ def get_sensmat_fisavg(ens, vals, ensfis, valsfis):
     return sensvec
 
 
-
-def get_sensmat_fisavg_corrected(ens, vals, ensfis, valsfis):
+def get_sensmat_fisavg_corrected(ens, vals, ensfis, valsfis, check_norm=True):
     """Correct SACS Jacobian calculation."""
     ord = np.argsort(ens)
     ens = ens[ord]
@@ -114,7 +106,6 @@ def get_sensmat_fisavg_corrected(ens, vals, ensfis, valsfis):
     # as lowest energy 2.53e-8 MeV
     fidx = np.searchsorted(ensfis[1:], ens[1]) + 1
 
-    lentmp = len(ensfis)
     ensfis = np.concatenate([ensfis, np.full(100, 0.)])
     valsfis = np.concatenate([valsfis, np.full(100, 0.)])
 
@@ -128,7 +119,6 @@ def get_sensmat_fisavg_corrected(ens, vals, ensfis, valsfis):
     #   raise ValueError('energies of excitation function and fission spectrum do not match')
 
     fl = 0.
-    sfl = 0.
     sensvec = np.full(len(vals), 0., dtype=float)
     for i in range(1, ulimidx2-1):
         fl = fl + valsfis[fidx-1+i]
@@ -162,10 +152,9 @@ def get_sensmat_fisavg_corrected(ens, vals, ensfis, valsfis):
     sensvec[-1] += valsfis[urealidx-1]
     sensvec /= fl
 
-    if not np.isclose(1., fl, atol=0, rtol=1e-4):
+    if check_norm and not np.isclose(1., fl, atol=0, rtol=1e-4):
         print('fission normalization: ' + str(fl))
         raise ValueError('fission spectrum not normalized')
 
     sensvec[ord] = sensvec.copy()
     return sensvec
-
