8be6972ea89557ea0daf5ccab141e0f4c4b864b2
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-02-15 16:47:43 +0100

    remove old code only kept for ad-hoc testing

--- gmapy/mappings/cross_section_fission_average_map.py
+++ gmapy/mappings/cross_section_fission_average_map.py
@@ -28,52 +28,23 @@ class CrossSectionFissionAverageMap:
         return np.array(expmask, dtype=bool)
 
     def propagate(self, datatable, refvals):
-        # keep temporary to test equivalence of new way
-        oldpreds = np.full(len(datatable), 0., dtype=float)
-        mapdic = self.__compute(datatable, refvals, what='propagate')
-        oldpreds[mapdic['idcs2']] = mapdic['propvals']
-        # new way
-        preds = self.__compute(datatable, refvals, what='propagate', new_way=True)
-        assert np.allclose(oldpreds, preds)
-        return preds
+        return self.__compute(datatable, refvals, what='propagate')
 
     def jacobian(self, datatable, refvals, ret_mat=False):
-        # keep temporary to test equivalence of new way
-        num_points = datatable.shape[0]
-        oldSdic = self.__compute(datatable, refvals, what='jacobian')
-        oldret = return_matrix(oldSdic['idcs1'], oldSdic['idcs2'], oldSdic['coeff'],
-                  dims = (num_points, num_points),
-                  how = 'csr' if ret_mat else 'dic')
-        # new way
-        Smat = self.__compute(datatable, refvals, what='jacobian', new_way=True)
-        ret = return_matrix_new(Smat, how='csr' if ret_mat else 'dic')
-        # compare
-        if not self._legacy_integration:
-            if ret_mat:
-                tmp1 = oldret.toarray()
-                tmp2 = ret.toarray()
-                assert np.allclose(tmp2[tmp1 != 0], tmp1[tmp1 != 0])
-            else:
-                tmp = oldret['x'] != 0
-                assert (np.any(oldret['idcs1'][tmp] != ret['idcs1'][tmp]) or
-                        np.any(oldret['idcs2'][tmp] != ret['idcs2'][tmp]) or
-                        not np.allclose(oldret['x'][tmp], ret['x'][tmp]))
-        return ret
+        Smat = self.__compute(datatable, refvals, what='jacobian')
+        return return_matrix_new(Smat, how='csr' if ret_mat else 'dic')
 
-    def __compute(self, datatable, refvals, what, new_way=False):
+    def __compute(self, datatable, refvals, what):
         has_legacy_fis = datatable.NODE.str.fullmatch('fis', na=False).any()
         has_modern_fis = datatable.NODE.str.fullmatch('fis_modern', na=False).any()
         if has_legacy_fis and has_modern_fis:
             raise ValueError('either legacy or modern fission spectrum, not both!')
         if has_legacy_fis:
-            if new_way:
-                return self.__new_legacy_compute(datatable, refvals, what)
-            else:
-                return self.__legacy_compute(datatable, refvals, what)
+            return self.__legacy_compute(datatable, refvals, what)
         elif has_modern_fis:
             return self.__modern_compute(datatable, refvals, what)
 
-    def __new_legacy_compute(self, datatable, refvals, what):
+    def __legacy_compute(self, datatable, refvals, what):
         assert what in ('propagate', 'jacobian')
         legacy_integration = self._legacy_integration
         fix_jacobian = self._fix_jacobian
@@ -153,116 +124,6 @@ class CrossSectionFissionAverageMap:
             return out.jacobian()
         else:
             raise ValueError(f'what "{what}" not implemented"')
-
-    def __legacy_compute(self, datatable, refvals, what):
-        assert what in ('propagate', 'jacobian')
-        legacy_integration = self._legacy_integration
-
-        idcs1 = np.empty(0, dtype=int)
-        idcs2 = np.empty(0, dtype=int)
-        coeff = np.empty(0, dtype=float)
-        propvals = np.empty(0, dtype=float)
-        concat = np.concatenate
-
-        priormask = (datatable['REAC'].str.match('MT:1-R1:', na=False) &
-                     datatable['NODE'].str.match('xsid_', na=False))
-        priormask = np.logical_or(priormask, datatable['NODE'].str.fullmatch('fis', na=False))
-        priortable = datatable[priormask]
-
-        expmask = self.is_responsible(datatable)
-        exptable = datatable[expmask]
-        expids = exptable['NODE'].unique()
-
-        # retrieve fission spectrum
-        fistable = priortable[priortable['NODE'].str.fullmatch('fis', na=False)]
-        ensfis = fistable['ENERGY'].to_numpy()
-        valsfis = fistable['PRIOR'].to_numpy()
-
-        if not legacy_integration:
-            # The fission spectrum values in the legacy GMA database
-            # are given as a histogram (piecewise rectangular function)
-            # where the spectrum value in each bin is divided by the
-            # energy bin size. For the new routine, where we interpret
-            # the spectrum point-wise, we therefore need to multiply
-            # by the energy bin size
-            sort_idcs = ensfis.argsort()
-            sorted_ensfis = ensfis[sort_idcs]
-            xdiff = np.diff(sorted_ensfis)
-            xmid = sorted_ensfis[:-1] + xdiff/2
-            sorted_scl = np.full(len(sorted_ensfis), 1.)
-            sorted_scl[1:-1] /= np.diff(xmid)
-            sorted_scl[0] /= (xdiff[0]/2)
-            sorted_scl[-1] /= (xdiff[-1]/2)
-            scl = np.empty(len(sorted_scl), dtype=float)
-            scl[sort_idcs] = sorted_scl
-            valsfis *= scl
-            # evaluate the normalization of the fission spectrum
-            normfact = 1./float(basic_integral_propagate(ensfis, valsfis,
-                                                         'lin-lin', maxord=16,
-                                                         rtol=1e-6))
-
-        for curexp in expids:
-            exptable_red = exptable[exptable['NODE'].str.fullmatch(curexp, na=False)]
-            if len(exptable_red) != 1:
-                raise IndexError('None or more than one rows associated with a ' +
-                        'fission average, which must not happen!')
-            curreac = exptable_red['REAC'].values[0]
-            preac = curreac.replace('MT:6-', 'MT:1-')
-            priortable_red = priortable[priortable['REAC'].str.fullmatch(preac, na=False)]
-            # abbreviate some variables
-            ens1 = priortable_red['ENERGY'].to_numpy()
-            vals1 = refvals[priortable_red.index]
-            idcs1red = priortable_red.index
-            idcs2red = exptable_red.index
-
-            if legacy_integration:
-                if what == 'propagate':
-                    curval = propagate_fisavg(ens1, vals1, ensfis, valsfis)
-                    idcs2 = concat([idcs2, idcs2red])
-                    propvals = concat([propvals, [curval]])
-                elif what == 'jacobian':
-                    if self._fix_jacobian:
-                        sensvec = get_sensmat_fisavg_corrected(ens1, vals1, ensfis, valsfis)
-                    else:
-                        sensvec = get_sensmat_fisavg(ens1, vals1, ensfis, valsfis)
-                    idcs1 = concat([idcs1, idcs1red])
-                    idcs2 = concat([idcs2, np.full(len(idcs1red), idcs2red, dtype=int)])
-                    coeff = concat([coeff, sensvec])
-            # modern (Romberg) integration
-            else:
-                if what == 'propagate':
-                    curval = basic_integral_of_product_propagate(
-                            [ens1, ensfis], [vals1, valsfis],
-                            ['lin-lin', 'lin-lin'], zero_outside=True,
-                            maxord=16, rtol=1e-6)
-                    curval = float(curval) * normfact
-                    idcs2 = concat([idcs2, idcs2red])
-                    propvals = concat([propvals, [curval]])
-                elif what == 'jacobian':
-                    sensvecs = get_basic_integral_of_product_sensmats(
-                            [ens1, ensfis], [vals1, valsfis],
-                            ['lin-lin', 'lin-lin'], zero_outside=True,
-                            maxord=16, rtol=1e-6)
-                    # because we assume that the fission spectrum is constant
-                    # we can ignore the sensitivity to it
-                    sensvec = np.ravel(sensvecs[0])
-                    sensvec *= normfact
-
-                    idcs1 = concat([idcs1, idcs1red])
-                    idcs2 = concat([idcs2, np.full(len(idcs1red), idcs2red, dtype=int)])
-                    coeff = concat([coeff, sensvec])
-
-        retdic = {}
-        if what == 'jacobian':
-            retdic['idcs1'] = idcs1
-            retdic['idcs2'] = idcs2
-            retdic['coeff'] = coeff
-        elif what == 'propagate':
-            retdic['idcs2'] = idcs2
-            retdic['propvals'] = propvals
-
-        return retdic
-
 
     def __modern_compute(self, datatable, refvals, what):
         idcs1 = np.empty(0, dtype=int)
