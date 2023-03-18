340e8da7618fbd601f595367a3855be0f89ee8cc
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-03-18 16:45:19 +0100

    start using specialized integration funs

--- gmapy/mappings/basic_integral_maps.py
+++ gmapy/mappings/basic_integral_maps.py
@@ -219,76 +219,89 @@ def get_basic_integral_sensmat(x, y, interp_type='lin-lin',
 
 def basic_integral_of_product_propagate(xlist, ylist, interplist,
                                         zero_outside=False, **kwargs):
-    def propfun(x):
-        return basic_product_propagate(xlist, ylist, x,
-                                       interplist, zero_outside)
-    xref = np.unique(np.concatenate(xlist))
-    ret = compute_romberg_integral(xref, propfun, **kwargs)
-    ret = np.array([ret])
-    return ret
+    if (len(xlist) == 2 and
+            np.all(interplist[0] == 'lin-lin') and
+            np.all(interplist[1] == 'lin-lin')):
+        ret = _integrate_product_lin_lin(
+            xlist[0], ylist[0], xlist[1], ylist[1], zero_outside)
+        return np.array([ret], dtype=float)
+    else:
+        def propfun(x):
+            return basic_product_propagate(xlist, ylist, x,
+                                           interplist, zero_outside)
+        xref = np.unique(np.concatenate(xlist))
+        ret = compute_romberg_integral(xref, propfun, **kwargs)
+        ret = np.array([ret])
+        return ret
 
 
 def get_basic_integral_of_product_sensmats(xlist, ylist, interplist,
                                            zero_outside=False, **kwargs):
+    if (len(xlist) == 2 and
+            np.all(interplist[0] == 'lin-lin') and
+            np.all(interplist[1] == 'lin-lin')):
+        ret = _integrate_product_lin_lin_sensmats(
+            xlist[0], ylist[0], xlist[1], ylist[1], zero_outside)
+        return ret
+    else:
+        def propfun(x):
+            return basic_product_propagate(xlist, ylist, x,
+                                           interplist, zero_outside)
+        # The function sensfun is introduced in order to
+        # cache the results of get_basic_product_sensmats called
+        # in cur_dpropfun because it will be called several times
+        # with identical arguments there.
+        @lru_cache(maxsize=64)
+        def sensfun(x):
+            x = np.array(x)
+            return get_basic_product_sensmats(xlist_ref, ylist_ref, x, interplist,
+                                              zero_outside)
+        def generate_dpropfun(i):
+            def cur_dpropfun(x):
+                # convert to tuple because ndarrays are
+                # not hashable and lru_cache would fail
+                tx = tuple(x)
+                S_list = sensfun(tx)
+                S = S_list[i]
+                coeffs1, coeffs2 = extract_partial_derivatives(S, xlist_ref[i], x)
+                return (coeffs1, coeffs2)
+            return cur_dpropfun
+        # determine a common mesh
+        min_x_list = np.array([np.min(tx) for tx in xlist])
+        max_x_list = np.array([np.max(tx) for tx in xlist])
+        if not zero_outside:
+            if (not np.all(min_x_list == min_x_list[0]) or
+                not np.all(max_x_list == max_x_list[0])):
+                raise ValueError('The x-limits of the meshes do not coincide')
 
-    def propfun(x):
-        return basic_product_propagate(xlist, ylist, x,
-                                       interplist, zero_outside)
-    # The function sensfun is introduced in order to
-    # cache the results of get_basic_product_sensmats called
-    # in cur_dpropfun because it will be called several times
-    # with identical arguments there.
-    @lru_cache(maxsize=64)
-    def sensfun(x):
-        x = np.array(x)
-        return get_basic_product_sensmats(xlist_ref, ylist_ref, x, interplist,
-                                          zero_outside)
-    def generate_dpropfun(i):
-        def cur_dpropfun(x):
-            # convert to tuple because ndarrays are
-            # not hashable and lru_cache would fail
-            tx = tuple(x)
-            S_list = sensfun(tx)
-            S = S_list[i]
-            coeffs1, coeffs2 = extract_partial_derivatives(S, xlist_ref[i], x)
-            return (coeffs1, coeffs2)
-        return cur_dpropfun
-    # determine a common mesh
-    min_x_list = np.array([np.min(tx) for tx in xlist])
-    max_x_list = np.array([np.max(tx) for tx in xlist])
-    if not zero_outside:
-        if (not np.all(min_x_list == min_x_list[0]) or
-            not np.all(max_x_list == max_x_list[0])):
-            raise ValueError('The x-limits of the meshes do not coincide')
+        min_x = np.max([np.min(tx) for tx in xlist])
+        max_x = np.min([np.max(tx) for tx in xlist])
+        xref = np.unique(np.concatenate(xlist))
+        xref = xref[np.logical_and(xref >= min_x, xref <= max_x)]
+        # use the common mesh for each basic mapping
+        xlist_ref = []
+        ylist_ref = []
+        interplist_ref = []
+        for x, y, interp in zip(xlist, ylist, interplist):
+            cury = basic_propagate(x, y, xref, interp, zero_outside)
+            tmpord = np.argsort(x)
+            idcs = np.searchsorted(x[tmpord], xref)
+            idcs = tmpord[idcs]
+            if isinstance(interp, str):
+                interp = np.full(len(x), interp)
+            curinterp = np.array(interp, copy=False)[idcs]
+            xlist_ref.append(xref)
+            ylist_ref.append(cury)
+            interplist_ref.append(curinterp)
+        # compute the sensitivity matrix for each inddividual contribution
+        Smat_list = []
+        for i in range(len(xlist)):
+            cur_dfun = generate_dpropfun(i)
+            cur_Smat =  compute_romberg_integral(xref, propfun,
+                                                 dfun=cur_dfun, **kwargs)
+            curSref = get_basic_sensmat(xlist[i], ylist[i], xref, interplist[i],
+                                        zero_outside)
+            cur_Smat = np.array([cur_Smat]) @ curSref
+            Smat_list.append(cur_Smat)
 
-    min_x = np.max([np.min(tx) for tx in xlist])
-    max_x = np.min([np.max(tx) for tx in xlist])
-    xref = np.unique(np.concatenate(xlist))
-    xref = xref[np.logical_and(xref >= min_x, xref <= max_x)]
-    # use the common mesh for each basic mapping
-    xlist_ref = []
-    ylist_ref = []
-    interplist_ref = []
-    for x, y, interp in zip(xlist, ylist, interplist):
-        cury = basic_propagate(x, y, xref, interp, zero_outside)
-        tmpord = np.argsort(x)
-        idcs = np.searchsorted(x[tmpord], xref)
-        idcs = tmpord[idcs]
-        if isinstance(interp, str):
-            interp = np.full(len(x), interp)
-        curinterp = np.array(interp, copy=False)[idcs]
-        xlist_ref.append(xref)
-        ylist_ref.append(cury)
-        interplist_ref.append(curinterp)
-    # compute the sensitivity matrix for each inddividual contribution
-    Smat_list = []
-    for i in range(len(xlist)):
-        cur_dfun = generate_dpropfun(i)
-        cur_Smat =  compute_romberg_integral(xref, propfun,
-                                             dfun=cur_dfun, **kwargs)
-        curSref = get_basic_sensmat(xlist[i], ylist[i], xref, interplist[i],
-                                    zero_outside)
-        cur_Smat = np.array([cur_Smat]) @ curSref
-        Smat_list.append(cur_Smat)
-
-    return Smat_list
+        return Smat_list
