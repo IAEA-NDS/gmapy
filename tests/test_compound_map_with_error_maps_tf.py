e5b6fe2887f95d979af1526adf597e9f2323dcc8
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-06-07 16:02:39 +0200

    add test case for tensorflow EnergyDependentUSUMap

--- tests/test_compound_map_with_error_maps_tf.py
+++ tests/test_compound_map_with_error_maps_tf.py
@@ -6,6 +6,7 @@ from gmapy.gma_database_class import GMADatabase
 from gmapy.mappings.compound_map import CompoundMap
 from gmapy.mappings.compound_map_tf \
     import CompoundMap as CompoundMapTF
+from gmapy.mappings.energy_dependent_usu_map import attach_endep_usu_df
 
 
 class TestCompoundMapWithErrorMapsTF(unittest.TestCase):
@@ -16,6 +17,24 @@ class TestCompoundMapWithErrorMapsTF(unittest.TestCase):
                 'data-2017-07-26.gma').resolve().as_posix()
         cls._dbpath = dbpath
         cls._gmadb = GMADatabase(dbpath, use_relative_errors=True)
+
+    def test_proper_propagation_of_endep_usu_errors(self):
+        dt = self._gmadb.get_datatable()
+        dt = attach_endep_usu_df(
+            dt, ['MT:1-R1:8'], [1, 10, 18], [0.1, 0.2, 0.3]
+        )
+        refvals = dt.PRIOR.to_numpy(copy=True)
+        compmap = CompoundMap(dt)
+        compmap_tf = CompoundMapTF(dt)
+        sel = (dt.NODE.str.match('endep_usu_1027') &
+               (dt.ENERGY.isin([1.0, 10.0, 18.0])))
+        idcs = dt.index[sel]
+        assert len(idcs) == 3
+        refvals[idcs] = np.array((0.11, 0.34, -0.12), dtype=float)
+        refvals_tf = tf.Variable(refvals, dtype=tf.float64)
+        propvals = compmap.propagate(refvals)
+        propvals_tf = compmap_tf(refvals_tf)
+        self.assertTrue(np.allclose(propvals, propvals_tf.numpy()))
 
     def test_proper_propagation_of_relative_errors(self):
         dt = self._gmadb.get_datatable()
