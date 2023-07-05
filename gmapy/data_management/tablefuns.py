ae04c5ec21ba4c1e6961170a5d30a498715e7072
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-07-05 14:39:53 +0200

    simplify tablefuns.py

--- gmapy/data_management/tablefuns.py
+++ gmapy/data_management/tablefuns.py
@@ -4,15 +4,7 @@ from collections import OrderedDict
 from .datablock_api import (
     dataset_iterator
 )
-from .dataset_api import (
-    get_dataset_identifier,
-    get_quantity_type,
-    get_reaction_identifiers,
-    get_measured_values,
-    get_incident_energies,
-    get_authors_string,
-    get_publication_string
-)
+from . import dataset_api as dsapi
 from . import priorblock_api as priorapi
 
 
@@ -41,22 +33,22 @@ def create_dataframe_from_experiment_dataset(
     dataset, datablock_index, dataset_index
 ):
     ds = dataset
-    quant_part = 'MT:' + str(get_quantity_type(ds))
-    reac_ids = get_reaction_identifiers(ds)
+    quant_part = 'MT:' + str(dsapi.get_quantity_type(ds))
+    reac_ids = dsapi.get_reaction_identifiers(ds)
     reac_part = ''.join(
         ['-R%d:%d' % (i+1, r) for i, r in enumerate(reac_ids)]
     )
     curdf = pd.DataFrame.from_dict({
-        'NODE': 'exp_' + str(get_dataset_identifier(ds)),
+        'NODE': 'exp_' + str(dsapi.get_dataset_identifier(ds)),
         'REAC':   quant_part + reac_part,
-        'ENERGY': get_incident_energies(ds),
+        'ENERGY': dsapi.get_incident_energies(ds),
         'PRIOR':  0.,
         'UNC':    np.nan,
-        'DATA':   get_measured_values(ds),
+        'DATA':   dsapi.get_measured_values(ds),
         'DB_IDX': datablock_index,
         'DS_IDX': dataset_index,
-        'AUTHOR': get_authors_string(ds).strip(),
-        'PUBREF': get_publication_string(ds).strip()
+        'AUTHOR': dsapi.get_authors_string(ds),
+        'PUBREF': dsapi.get_publication_string(ds)
     })
     return curdf
 
@@ -65,20 +57,15 @@ def create_experiment_table(datablock_list):
     """Extract experiment dataframe from datablock list."""
     df_list = []
     for dbidx, db in enumerate(datablock_list):
-        if db['type'] not in (
-            'legacy-experiment-datablock', 'simple-experiment-datablock'
-        ):
-            raise ValueError('Unsupported type of datablock')
-        for dsidx, ds in enumerate(db['datasets']):
-            if ds['type'] == 'legacy-experiment-dataset':
-                curdf = create_dataframe_from_experiment_dataset(
-                    ds, dbidx, dsidx
-                )
-            else:
-                raise ValueError('Unsupported type of dataset')
+        datasets = tuple(dataset_iterator(db))
+        for dsidx, ds in enumerate(datasets):
+            curdf = create_dataframe_from_experiment_dataset(
+                ds, dbidx, dsidx
+            )
             df_list.append(curdf)
 
     expdf = pd.concat(df_list, ignore_index=True)
-    cols = ['NODE', 'REAC', 'ENERGY', 'DATA', 'DB_IDX', 'DS_IDX', 'AUTHOR', 'PUBREF']
+    cols = ['NODE', 'REAC', 'ENERGY', 'DATA',
+            'DB_IDX', 'DS_IDX', 'AUTHOR', 'PUBREF']
     expdf = expdf.reindex(columns=cols)
     return expdf
