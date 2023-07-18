3bbf3245b8c10041f78a0dfb42be935ef61f4d62
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-07-18 08:45:43 +0200

    restructure cross_section_ratio_map_tf for faster jacobian calc

--- gmapy/mappings/cross_section_ratio_map_tf.py
+++ gmapy/mappings/cross_section_ratio_map_tf.py
@@ -19,6 +19,16 @@ class CrossSectionRatioMap(tf.Module):
         if selcol is None:
             selcol = InputSelectorCollection()
         self._selcol = selcol
+        # member vars
+        self._src_len = None
+        self._tar_len = None
+        self._src_idcs_list = []
+        self._tar_idcs_list = []
+        self._propfun_list = []
+        self._aux_list = []
+        self._jacfun_list = []
+        self._prepared_propagate = False
+        self._prepared_jacobian = False
 
     @classmethod
     def is_applicable(cls, datatable):
@@ -27,10 +37,14 @@ class CrossSectionRatioMap(tf.Module):
             datatable['NODE'].str.match('exp_', na=False)
         ).any()
 
-    @tf.function
-    def __call__(self, inputs):
+    def _prepare_propagate(self):
+        if self._prepared_propagate is True:
+            return
+
         priortable, exptable, src_len, tar_len = \
             prepare_prior_and_exptable(self._datatable, self._reduce)
+        self._src_len = src_len
+        self._tar_len = tar_len
 
         priormask = (priortable['REAC'].str.match('MT:1-R1:', na=False) &
                      priortable['NODE'].str.match('xsid_', na=False))
@@ -43,8 +57,6 @@ class CrossSectionRatioMap(tf.Module):
 
         exptable = exptable[expmask]
         reacs = exptable['REAC'].unique()
-        selcol = self._selcol
-        out_list = []
         for curreac in reacs:
             # obtian the involved reactions
             string_groups = curreac.split('-')
@@ -64,14 +76,114 @@ class CrossSectionRatioMap(tf.Module):
             src_en2 = np.array(priortable_red2['ENERGY'])
             tar_idcs = np.array(exptable_red.index)
             tar_en = np.array(exptable_red['ENERGY'])
+            propfun = self._generate_atomic_propagate(src_en1, src_en2, tar_en)
+            self._add_lists(
+                (src_idcs1, src_idcs2), tar_idcs, propfun,
+                (src_en1, src_en2, tar_en)
+            )
+        self._prepared_propagate = True
 
-            inpvar1 = selcol.define_selector(src_idcs1)(inputs)
-            inpvar2 = selcol.define_selector(src_idcs2)(inputs)
-            inpvar1_int = PiecewiseLinearInterpolation(src_en1, tar_en)(inpvar1)
-            inpvar2_int = PiecewiseLinearInterpolation(src_en2, tar_en)(inpvar2)
-            tmpres = inpvar1_int / inpvar2_int
-            outvar = Distributor(tar_idcs, tar_len)(tmpres)
+    def _prepare_jacobian(self):
+        if self._prepared_jacobian:
+            return
+        self._prepare_propagate()
+        it = self._lists_iterator()
+        jacfun_list = []
+        for _, _, propfun, _, _ in it:
+            jacfun = self._generate_atomic_jacobian(propfun)
+            jacfun_list.append(jacfun)
+        self._jacfun_list = jacfun_list
+        self._prepared_jacobian = True
+
+    def _add_lists(
+        self, src_idcs_list, tar_idcs, propfun, jacfun=None, aux_list=None
+    ):
+        self._src_idcs_list.append(src_idcs_list)
+        self._tar_idcs_list.append(tar_idcs)
+        self._aux_list.append(aux_list)
+        self._propfun_list.append(propfun)
+        self._jacfun_list.append(jacfun)
+
+    def _lists_iterator(self):
+        it = zip(
+            self._src_idcs_list, self._tar_idcs_list,
+            self._propfun_list, self._jacfun_list, self._aux_list
+        )
+        for src_idcs_list, tar_idcs_list, propfun, jacfun, aux_list in it:
+            yield src_idcs_list, tar_idcs_list, propfun, jacfun, aux_list
+
+    @tf.function
+    def __call__(self, inputs):
+        self._prepare_propagate()
+        selcol = self._selcol
+        out_list = []
+        it = self._lists_iterator()
+        for src_idcs_list, tar_idcs, propfun, _, _ in it:
+            inpvars = tuple(
+                selcol.define_selector(src_idcs)(inputs)
+                for src_idcs in src_idcs_list
+            )
+            tmpres = propfun(*inpvars)
+            outvar = Distributor(tar_idcs, self._tar_len)(tmpres)
             out_list.append(outvar)
 
         res = tf.add_n(out_list)
         return res
+
+    @tf.function
+    def jacobian(self, inputs):
+        self._prepare_jacobian()
+        selcol = self._selcol
+        res = tf.sparse.SparseTensor(
+            indices=[[0, 0]],
+            values=tf.zeros(shape=(1,), dtype=tf.float64),
+            dense_shape=(self._tar_len, self._src_len)
+        )
+        it = self._lists_iterator()
+        for src_idcs_list, tar_idcs, _, jacfun, _ in it:
+            inpvars = []
+            for src_idcs in src_idcs_list:
+                cur_inpvar = selcol.define_selector(src_idcs)(inputs)
+                inpvars.append(cur_inpvar)
+            jac_list = jacfun(*inpvars)
+            for src_idcs, jac in zip(src_idcs_list, jac_list):
+                st = tf.sparse.from_dense(jac)
+                src_idcs_tf = tf.constant(src_idcs, dtype=tf.int64)
+                tar_idcs_tf = tf.constant(tar_idcs, dtype=tf.int64)
+                slice1 = tf.reshape(
+                    tf.slice(st.indices, [0, 1], [-1, 1]),
+                    shape=(-1,)
+                )
+                s1 = tf.gather(src_idcs_tf, slice1)
+                slice2 = tf.reshape(
+                    tf.slice(st.indices, [0, 0], [-1, 1]),
+                    shape=(-1,)
+                )
+                t1 = tf.gather(tar_idcs_tf, slice2)
+                z = tf.stack((t1, s1), axis=1)
+                st = tf.sparse.SparseTensor(
+                    indices=z,
+                    values=st.values,
+                    dense_shape=(self._tar_len, self._src_len)
+                )
+                res = tf.sparse.add(res, st)
+
+        return res
+
+    def _generate_atomic_propagate(self, src_en1, src_en2, tar_en):
+        def _atomic_propagate(inpvar1, inpvar2):
+            inpvar1_int = PiecewiseLinearInterpolation(src_en1, tar_en)(inpvar1)
+            inpvar2_int = PiecewiseLinearInterpolation(src_en2, tar_en)(inpvar2)
+            tmpres = inpvar1_int / inpvar2_int
+            return tmpres
+        return _atomic_propagate
+
+    def _generate_atomic_jacobian(self, atomic_propagate_fun):
+        def _atomic_jacobian(*inpvars):
+            with tf.GradientTape() as tape:
+                for iv in inpvars:
+                    tape.watch(iv)
+                predvals = atomic_propagate_fun(*inpvars)
+            jac = tape.jacobian(predvals, inpvars)
+            return jac
+        return _atomic_jacobian
