7d6667f608dc758aca4e04882031798519366acc
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-07-18 15:25:34 +0200

    make tf CrossSectionShapeMap derived class of CrossSectionBaseMap

--- gmapy/mappings/cross_section_shape_map_tf.py
+++ gmapy/mappings/cross_section_shape_map_tf.py
@@ -1,24 +1,11 @@
 import numpy as np
-import tensorflow as tf
-from .priortools import prepare_prior_and_exptable
+from .cross_section_base_map_tf import CrossSectionBaseMap
 from .mapping_elements_tf import (
-    PiecewiseLinearInterpolation,
-    InputSelectorCollection,
-    Distributor
+    PiecewiseLinearInterpolation
 )
 
 
-class CrossSectionShapeMap(tf.Module):
-
-    def __init__(self, datatable, selcol=None, reduce=True):
-        super().__init__()
-        if not self.is_applicable(datatable):
-            raise TypeError('CrossSectionMap not applicable')
-        self._datatable = datatable
-        self._reduce = reduce
-        if selcol is None:
-            selcol = InputSelectorCollection()
-        self._selcol = selcol
+class CrossSectionShapeMap(CrossSectionBaseMap):
 
     @classmethod
     def is_applicable(cls, datatable):
@@ -27,18 +14,11 @@ class CrossSectionShapeMap(tf.Module):
             datatable['NODE'].str.match('exp_', na=False)
         ).any()
 
-    @tf.function
-    def __call__(self, inputs):
-        priortable, exptable, src_len, tar_len = \
-            prepare_prior_and_exptable(self._datatable, self._reduce)
-
+    def _prepare_propagate(self, priortable, exptable):
         isresp = np.array(exptable['REAC'].str.match('MT:2-R1:', na=False) &
                           exptable['NODE'].str.match('exp_', na=False))
 
         reacs = exptable.loc[isresp, 'REAC'].unique()
-
-        selcol = self._selcol
-        out_list = []
         for curreac in reacs:
             priormask = ((priortable['REAC'].str.fullmatch(curreac.replace('MT:2','MT:1'), na=False)) &
                          priortable['NODE'].str.match('xsid_', na=False))
@@ -47,8 +27,6 @@ class CrossSectionShapeMap(tf.Module):
                                      exptable['NODE'].str.match('exp_'))]
             ens1 = np.array(priortable_red['ENERGY'])
             idcs1red = np.array(priortable_red.index)
-
-            inpvar = selcol.define_selector(idcs1red)(inputs)
             # loop over the datasets
             dataset_ids = exptable_red['NODE'].unique()
             for dataset_id in dataset_ids:
@@ -59,16 +37,14 @@ class CrossSectionShapeMap(tf.Module):
                 if (len(norm_index) != 1):
                     raise IndexError('There are ' + str(len(norm_index)) +
                         ' normalization factors in prior for dataset ' + str(dataset_id))
-
-                norm_fact = selcol.define_selector(norm_index)(inputs)
                 # abbreviate some variables
                 ens2 = np.array(exptable_ds['ENERGY'])
                 idcs2red = np.array(exptable_ds.index)
+                propfun = self._generate_atomic_propagate(ens1, ens2)
+                self._add_lists((idcs1red, norm_index), idcs2red, propfun)
 
-                inpvar_int = PiecewiseLinearInterpolation(ens1, ens2)(inpvar)
-                prod = norm_fact * inpvar_int
-                outvar = Distributor(idcs2red, tar_len)(prod)
-                out_list.append(outvar)
-
-        res = tf.add_n(out_list)
-        return res
+    def _generate_atomic_propagate(self, ens1, ens2):
+        def _atomic_propagate(inpvar, norm_fact):
+            inpvar_int = PiecewiseLinearInterpolation(ens1, ens2)(inpvar)
+            return norm_fact * inpvar_int
+        return _atomic_propagate
