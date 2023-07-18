cce5ba37a612ff959deac7b1a1877cb5c2adee48
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-07-18 15:05:34 +0200

    make CrossSectionRatioShapeMap derived class of CrossSectionBaseMap

--- gmapy/mappings/cross_section_ratio_shape_map_tf.py
+++ gmapy/mappings/cross_section_ratio_shape_map_tf.py
@@ -1,24 +1,11 @@
 import numpy as np
-import tensorflow as tf
-from .priortools import prepare_prior_and_exptable
+from .cross_section_base_map_tf import CrossSectionBaseMap
 from .mapping_elements_tf import (
-    PiecewiseLinearInterpolation,
-    InputSelectorCollection,
-    Distributor
+    PiecewiseLinearInterpolation
 )
 
 
-class CrossSectionRatioShapeMap(tf.Module):
-
-    def __init__(self, datatable, selcol=None, reduce=True):
-        super().__init__()
-        if not self.is_applicable(datatable):
-            raise TypeError('CrossSectionRatioMap not applicable')
-        self._datatable = datatable
-        self._reduce = reduce
-        if selcol is None:
-            selcol = InputSelectorCollection()
-        self._selcol = selcol
+class CrossSectionRatioShapeMap(CrossSectionBaseMap):
 
     @classmethod
     def is_applicable(cls, datatable):
@@ -27,11 +14,7 @@ class CrossSectionRatioShapeMap(tf.Module):
             datatable['NODE'].str.match('exp_', na=False)
         ).any()
 
-    @tf.function
-    def __call__(self, inputs):
-        priortable, exptable, src_len, tar_len = \
-            prepare_prior_and_exptable(self._datatable, self._reduce)
-
+    def _prepare_propagate(self, priortable, exptable):
         priormask = (priortable['REAC'].str.match('MT:1-R1:', na=False) &
                      priortable['NODE'].str.match('xsid_', na=False))
         priormask = np.logical_or(priormask, priortable['NODE'].str.match('norm_', na=False))
@@ -43,9 +26,6 @@ class CrossSectionRatioShapeMap(tf.Module):
 
         exptable = exptable[expmask]
         reacs = exptable['REAC'].unique()
-
-        selcol = self._selcol
-        out_list = []
         for curreac in reacs:
             # obtian the involved reactions
             string_groups = curreac.split('-')
@@ -65,9 +45,6 @@ class CrossSectionRatioShapeMap(tf.Module):
             exptable_red = exptable[exptable['REAC'].str.fullmatch(curreac, na=False)]
             datasets = exptable_red['NODE'].unique()
 
-            inpvar1 = selcol.define_selector(src_idcs1)(inputs)
-            inpvar2 = selcol.define_selector(src_idcs2)(inputs)
-
             for ds in datasets:
                 tar_idcs = np.array(
                     exptable_red[exptable_red['NODE'].str.fullmatch(ds, na=False)].index
@@ -75,10 +52,6 @@ class CrossSectionRatioShapeMap(tf.Module):
                 tar_en = np.array(
                     exptable_red[exptable_red['NODE'].str.fullmatch(ds, na=False)]['ENERGY']
                 )
-                inpvar1_int = PiecewiseLinearInterpolation(src_en1, tar_en)(inpvar1)
-                inpvar2_int = PiecewiseLinearInterpolation(src_en2, tar_en)(inpvar2)
-                ratio = inpvar1_int / inpvar2_int
-
                 # obtain normalization and position in priortable
                 normstr = ds.replace('exp_', 'norm_')
                 norm_index = np.array(
@@ -86,11 +59,15 @@ class CrossSectionRatioShapeMap(tf.Module):
                 )
                 if len(norm_index) != 1:
                     raise IndexError('Exactly one normalization factor must be present for a dataset')
+                propfun = self._generate_atomic_propagate(src_en1, src_en2, tar_en)
+                self._add_lists(
+                    (src_idcs1, src_idcs2, norm_index), tar_idcs, propfun
+                )
 
-                norm_fact = selcol.define_selector(norm_index)(inputs)
-                mult_res = ratio * norm_fact
-                outvar = Distributor(tar_idcs, tar_len)(mult_res)
-                out_list.append(outvar)
-
-        res = tf.add_n(out_list)
-        return res
+    def _generate_atomic_propagate(self, src_en1, src_en2, tar_en):
+        def _atomic_propagate(inpvar1, inpvar2, norm_fact):
+            inpvar1_int = PiecewiseLinearInterpolation(src_en1, tar_en)(inpvar1)
+            inpvar2_int = PiecewiseLinearInterpolation(src_en2, tar_en)(inpvar2)
+            ratio = inpvar1_int / inpvar2_int
+            return ratio * norm_fact
+        return _atomic_propagate
