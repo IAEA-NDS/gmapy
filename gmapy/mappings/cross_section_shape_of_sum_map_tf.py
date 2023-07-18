e5b3f46f33b90e35c0cf27afde41003dc1721f94
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-07-18 16:11:04 +0200

    make CrossSectionShapeOfSumMap derived class of CrossSectionBaseMap

--- gmapy/mappings/cross_section_shape_of_sum_map_tf.py
+++ gmapy/mappings/cross_section_shape_of_sum_map_tf.py
@@ -1,24 +1,12 @@
 import numpy as np
 import tensorflow as tf
-from .priortools import prepare_prior_and_exptable
+from .cross_section_base_map_tf import CrossSectionBaseMap
 from .mapping_elements_tf import (
-    PiecewiseLinearInterpolation,
-    InputSelectorCollection,
-    Distributor
+    PiecewiseLinearInterpolation
 )
 
 
-class CrossSectionShapeOfSumMap(tf.Module):
-
-    def __init__(self, datatable, selcol=None, reduce=True):
-        super().__init__()
-        if not self.is_applicable(datatable):
-            raise TypeError('CrossSectionRatioMap not applicable')
-        self._datatable = datatable
-        self._reduce = reduce
-        if selcol is None:
-            selcol = InputSelectorCollection()
-        self._selcol = selcol
+class CrossSectionShapeOfSumMap(CrossSectionBaseMap):
 
     @classmethod
     def is_applicable(cls, datatable):
@@ -27,11 +15,7 @@ class CrossSectionShapeOfSumMap(tf.Module):
             datatable['NODE'].str.match('exp_', na=False)
         ).any()
 
-    @tf.function
-    def __call__(self, inputs):
-        priortable, exptable, src_len, tar_len = \
-            prepare_prior_and_exptable(self._datatable, self._reduce)
-
+    def _prepare_propagate(self, priortable, exptable):
         priormask = (priortable['REAC'].str.match('MT:1-R1:', na=False) &
                      priortable['NODE'].str.match('xsid_', na=False))
         priormask = np.logical_or(priormask, priortable['NODE'].str.match('norm_', na=False))
@@ -43,9 +27,6 @@ class CrossSectionShapeOfSumMap(tf.Module):
 
         exptable = exptable[expmask]
         reacs = exptable['REAC'].unique()
-
-        selcol = self._selcol
-        out_list = []
         for curreac in reacs:
             # obtian the involved reactions
             reac_groups = curreac.split('-')[1:]
@@ -59,12 +40,6 @@ class CrossSectionShapeOfSumMap(tf.Module):
             # some abbreviations
             src_idcs_list = [np.array(pt.index) for pt in priortable_reds]
             src_en_list = [np.array(pt['ENERGY']) for pt in priortable_reds]
-
-            cvars = [
-                selcol.define_selector(idcs)(inputs)
-                for idcs in src_idcs_list
-            ]
-
             # retrieve relevant rows in exptable
             exptable_red = exptable[exptable['REAC'].str.fullmatch(curreac, na=False)]
             datasets = exptable_red['NODE'].unique()
@@ -83,15 +58,16 @@ class CrossSectionShapeOfSumMap(tf.Module):
                 )
                 if len(norm_index) != 1:
                     raise IndexError('Exactly one normalization factor must be present for a dataset')
+                propfun = self._generate_atomic_propagate(src_en_list, tar_en)
+                ext_src_idcs_list = src_idcs_list + [norm_index]
+                self._add_lists(ext_src_idcs_list, tar_idcs, propfun)
 
-                norm_fact = selcol.define_selector(norm_index)(inputs)
-                cvars_int = []
-                for cv, src_en in zip(cvars, src_en_list):
-                    cvars_int.append(PiecewiseLinearInterpolation(src_en, tar_en)(cv))
-
-                tmpres = tf.add_n(cvars_int) * norm_fact
-                outvar = Distributor(tar_idcs, tar_len)(tmpres)
-                out_list.append(outvar)
-
-        res = tf.add_n(out_list)
-        return res
+    def _generate_atomic_propagate(self, src_en_list, tar_en):
+        def _atomic_propagate(*cvars_and_norm_fact):
+            cvars = cvars_and_norm_fact[:-1]
+            norm_fact = cvars_and_norm_fact[-1]
+            cvars_int = []
+            for cv, src_en in zip(cvars, src_en_list):
+                cvars_int.append(PiecewiseLinearInterpolation(src_en, tar_en)(cv))
+            return tf.add_n(cvars_int) * norm_fact
+        return _atomic_propagate
