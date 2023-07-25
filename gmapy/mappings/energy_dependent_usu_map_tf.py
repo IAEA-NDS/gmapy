6cd1de4808a7f755277b04c7adb5f1fc747022b2
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-07-25 21:22:54 +0200

    make EnergyDependentUSUMap derived class of CrossSectionModifierBaseMap

--- gmapy/mappings/energy_dependent_usu_map_tf.py
+++ gmapy/mappings/energy_dependent_usu_map_tf.py
@@ -1,27 +1,12 @@
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-from .priortools import prepare_prior_and_exptable
+from .cross_section_modifier_base_map_tf import CrossSectionModifierBaseMap
 from .mapping_elements_tf import (
-    InputSelectorCollection,
-    InputSelector,
-    Distributor,
     PiecewiseLinearInterpolation
 )
 
 
-class EnergyDependentUSUMap:
-
-    def __init__(self, datatable, orig_output, selcol=None, reduce=True):
-        super().__init__()
-        if not self.is_applicable(datatable):
-            raise TypeError('CrossSectionMap not applicable')
-        self._datatable = datatable
-        self._reduce = reduce
-        if selcol is None:
-            selcol = InputSelectorCollection()
-        self._selcol = selcol
-        self._orig_output = orig_output
+class EnergyDependentUSUMap(CrossSectionModifierBaseMap):
 
     @classmethod
     def is_applicable(cls, datatable):
@@ -30,18 +15,11 @@ class EnergyDependentUSUMap:
             (datatable['NODE'].str.match('endep_usu_', na=False)).any()
         ).any()
 
-    def __call__(self, inputs):
-        priortable, exptable, src_len, tar_len = \
-            prepare_prior_and_exptable(self._datatable, self._reduce)
-
+    def _prepare_propagate(self, priortable, exptable):
         priormask = priortable['NODE'].str.match('endep_usu_', na=False)
         priortable = priortable[priormask]
         expmask = exptable['NODE'].str.match('exp_', na=False)
         exptable = exptable[expmask]
-
-        orig_output = self._orig_output
-        selcol = self._selcol
-        out_list = []
         # establish the mapping for each USU component
         # associated with an experimental dataset
         rerr_expids = priortable['NODE'].str.extract(r'endep_usu_([0-9]+)$')
@@ -55,13 +33,14 @@ class EnergyDependentUSUMap:
             tar_idcs = np.array(tardt.index)
             src_ens = srcdt['ENERGY'].to_numpy()
             tar_ens = tardt['ENERGY'].to_numpy()
-            inpvar = selcol.define_selector(src_idcs)(inputs)
-            relerrs = PiecewiseLinearInterpolation(src_ens, tar_ens)(inpvar)
-            expquants = InputSelector(tar_idcs)(orig_output)
-            abserrs = relerrs * expquants
-            abserrs_dist = Distributor(tar_idcs, tar_len)(abserrs)
-            out_list.append(abserrs_dist)
+            propfun = self._generate_atomic_propagate(src_ens, tar_ens)
+            self._add_lists(
+                [src_idcs], tar_idcs, propfun
+            )
 
-        # target_indices = tmp_out.get_indices()
-        res = tf.add_n(out_list)
-        return res
+    def _generate_atomic_propagate(self, src_ens, tar_ens):
+        def _atomic_propagate(propvals, inpvar):
+            relerrs = PiecewiseLinearInterpolation(src_ens, tar_ens)(inpvar)
+            abserrs = relerrs * propvals
+            return abserrs
+        return _atomic_propagate
