5642059aeaa9931700fddb54c2b571c5c682d19c
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-07-27 12:41:40 +0200

    make EnergyDependentUSUMap derived class of CrossSectionBaseMap

--- gmapy/mappings/energy_dependent_usu_map.py
+++ gmapy/mappings/energy_dependent_usu_map.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from .cross_section_base_map import CrossSectionBaseMap
 from .mapping_elements import (
     InputSelectorCollection,
     Selector,
@@ -7,50 +8,21 @@ from .mapping_elements import (
     SumOfDistributors,
     LinearInterpolation
 )
-from .priortools import prepare_prior_and_exptable
 
 
-class EnergyDependentUSUMap:
+class EnergyDependentUSUMap(CrossSectionBaseMap):
 
     def __init__(self, datatable, distributor_like,
                  selcol=None, distsum=None, reduce=False):
         if type(distributor_like) not in (Distributor, SumOfDistributors):
             raise TypeError('distributor_like must be of class Distributor '
                             'or class SumOfDistributors')
-        self.__numrows = len(datatable)
-        if selcol is None:
-            selcol = InputSelectorCollection()
-        self.__input, self.__output = self.__prepare(
-            datatable, distributor_like, selcol, reduce
+        super().__init__(
+            datatable, selcol, distsum, reduce,
+            more_prepare_args={'distributor_like': distributor_like}
         )
-        if distsum is not None:
-            distsum.add_distributors(self.__output.get_distributors())
 
-    def is_responsible(self):
-        ret = np.full(self.__numrows, False)
-        if self.__output is not None:
-            idcs = self.__output.get_indices()
-            ret[idcs] = True
-        return ret
-
-    def propagate(self, refvals):
-        self.__input.assign(refvals)
-        return self.__output.evaluate()
-
-    def jacobian(self, refvals):
-        self.__input.assign(refvals)
-        return self.__output.jacobian()
-
-    def get_selectors(self):
-        return self.__input.get_selectors()
-
-    def get_distributors(self):
-        return self.__output.get_distributors()
-
-    def __prepare(self, datatable, distributor_like, selcol, reduce):
-        priortable, exptable, src_len, tar_len = \
-            prepare_prior_and_exptable(datatable, reduce)
-
+    def _prepare(self, priortable, exptable, selcol, distributor_like):
         priormask = priortable['NODE'].str.match('endep_usu_', na=False)
         priortable = priortable[priormask]
         expmask = exptable['NODE'].str.match('exp_', na=False)
@@ -79,11 +51,11 @@ class EnergyDependentUSUMap:
             srcdt = priortable[priortable['NODE'] == srcnode]
             tarnode = 'exp_' + expid
             tardt = exptable[exptable['NODE'] == tarnode]
-            inpvar = selcol.define_selector(srcdt.index, src_len)
+            inpvar = selcol.define_selector(srcdt.index, self._src_len)
             intres = LinearInterpolation(
                 inpvar, srcdt['ENERGY'], tardt['ENERGY'], zero_outside=True
             )
-            outvar = Distributor(intres, tardt.index, tar_len)
+            outvar = Distributor(intres, tardt.index, self._tar_len)
             inp.add_selector(inpvar)
             tmp_out.add_distributor(outvar)
             tar_idcs_list.append(np.array(tardt.index, copy=True))
@@ -93,7 +65,7 @@ class EnergyDependentUSUMap:
         expquants = aux_distsum
         abserrors = tmp_out * expquants
         abserrors_sel = Selector(abserrors, all_tar_idcs)
-        abserrors_dist = Distributor(abserrors_sel, all_tar_idcs, tar_len)
+        abserrors_dist = Distributor(abserrors_sel, all_tar_idcs, self._tar_len)
         out.add_distributor(abserrors_dist)
         return inp, out
 
