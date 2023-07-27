a42b74b17dd765b990f7daae5e9bb4aabe43163e
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-07-27 12:24:36 +0200

    make CrossSectionShapeOfRatioMap derived class of CrossSectionBaseMap

--- gmapy/mappings/cross_section_shape_of_ratio_map.py
+++ gmapy/mappings/cross_section_shape_of_ratio_map.py
@@ -1,4 +1,5 @@
 import numpy as np
+from .cross_section_base_map import CrossSectionBaseMap
 from .mapping_elements import (
     InputSelectorCollection,
     Replicator,
@@ -6,44 +7,11 @@ from .mapping_elements import (
     SumOfDistributors,
     LinearInterpolation,
 )
-from .priortools import prepare_prior_and_exptable
 
 
-class CrossSectionShapeOfRatioMap:
+class CrossSectionShapeOfRatioMap(CrossSectionBaseMap):
 
-    def __init__(self, datatable, selcol=None, distsum=None, reduce=False):
-        self.__numrows = len(datatable)
-        if selcol is None:
-            selcol = InputSelectorCollection()
-        self.__input, self.__output = self.__prepare(datatable, selcol, reduce)
-        if distsum is not None:
-            distsum.add_distributors(self.__output.get_distributors())
-
-    def is_responsible(self):
-        ret = np.full(self.__numrows, False)
-        if self.__output is not None:
-            idcs = self.__output.get_indices()
-            ret[idcs] = True
-        return ret
-
-    def propagate(self, refvals):
-        self.__input.assign(refvals)
-        return self.__output.evaluate()
-
-    def jacobian(self, refvals):
-        self.__input.assign(refvals)
-        return self.__output.jacobian()
-
-    def get_selectors(self):
-        return self.__input.get_selectors()
-
-    def get_distributors(self):
-        return self.__output.get_distributors()
-
-    def __prepare(self, datatable, selcol, reduce):
-        priortable, exptable, src_len, tar_len = \
-            prepare_prior_and_exptable(datatable, reduce)
-
+    def _prepare(self, priortable, exptable, selcol):
         priormask = (priortable['REAC'].str.match('MT:1-R1:', na=False) &
                      priortable['NODE'].str.match('xsid_', na=False))
         priormask = np.logical_or(priormask, priortable['NODE'].str.match('norm_', na=False))
@@ -85,9 +53,9 @@ class CrossSectionShapeOfRatioMap:
             src_en2 = priortable_red2['ENERGY']
             src_en3 = priortable_red3['ENERGY']
 
-            inpvar1 = selcol.define_selector(src_idcs1, src_len)
-            inpvar2 = selcol.define_selector(src_idcs2, src_len)
-            inpvar3 = selcol.define_selector(src_idcs3, src_len)
+            inpvar1 = selcol.define_selector(src_idcs1, self._src_len)
+            inpvar2 = selcol.define_selector(src_idcs2, self._src_len)
+            inpvar3 = selcol.define_selector(src_idcs3, self._src_len)
             inp.add_selectors([inpvar1, inpvar2, inpvar3])
 
             exptable_red = exptable[exptable['REAC'].str.fullmatch(curreac, na=False)]
@@ -102,7 +70,7 @@ class CrossSectionShapeOfRatioMap:
                 if len(norm_index) != 1:
                     raise IndexError('Exactly one normalization factor must be present for a dataset')
 
-                norm_fact = selcol.define_selector(norm_index, src_len)
+                norm_fact = selcol.define_selector(norm_index, self._src_len)
                 inp.add_selector(norm_fact)
                 norm_fact_rep = Replicator(norm_fact, len(tar_idcs))
 
@@ -110,7 +78,7 @@ class CrossSectionShapeOfRatioMap:
                 inpvar2_int = LinearInterpolation(inpvar2, src_en2, tar_en)
                 inpvar3_int = LinearInterpolation(inpvar3, src_en3, tar_en)
                 tmpres = norm_fact_rep * inpvar1_int / (inpvar2_int + inpvar3_int)
-                outvar = Distributor(tmpres, tar_idcs, tar_len)
+                outvar = Distributor(tmpres, tar_idcs, self._tar_len)
                 out.add_distributor(outvar)
 
         return inp, out
