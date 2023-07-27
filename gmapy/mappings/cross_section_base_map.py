246fa0e0b83eebbc1611ca3c1839ef2b96bc0475
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-07-27 12:09:44 +0200

    implement CrossSectionBaseMap

---
+++ gmapy/mappings/cross_section_base_map.py
@@ -0,0 +1,50 @@
+import numpy as np
+from .mapping_elements import (
+    InputSelectorCollection
+)
+from .priortools import prepare_prior_and_exptable
+
+
+class CrossSectionBaseMap:
+
+    def __init__(self, datatable, selcol=None, distsum=None, reduce=False):
+        self.__numrows = len(datatable)
+        if selcol is None:
+            selcol = InputSelectorCollection()
+        self._input, self._output = self._base_prepare(datatable, selcol, reduce)
+        if distsum is not None:
+            distsum.add_distributors(self._output.get_distributors())
+
+    def is_responsible(self):
+        ret = np.full(self.__numrows, False)
+        if self._output is not None:
+            idcs = self._output.get_indices()
+            ret[idcs] = True
+        return ret
+
+    def propagate(self, refvals):
+        self._input.assign(refvals)
+        return self._output.evaluate()
+
+    def jacobian(self, refvals):
+        self._input.assign(refvals)
+        return self._output.jacobian()
+
+    def get_selectors(self):
+        return self._input.get_selectors()
+
+    def get_distributors(self):
+        return self._output.get_distributors()
+
+    def _base_prepare(self, datatable, selcol, reduce):
+        priortable, exptable, src_len, tar_len = \
+            prepare_prior_and_exptable(datatable, reduce)
+        self._src_len = src_len
+        self._tar_len = tar_len
+        inp, out = self._prepare(priortable, exptable, selcol)
+        return inp, out
+
+    def _prepare(self, priortable, exptable, selcol):
+        raise NotImplementedError(
+            'Please implement this method in the derived class'
+        )
