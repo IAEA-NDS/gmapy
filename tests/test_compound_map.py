6c0a22f5353797b60f7eebc68f164a8c254d2a6f
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-08-02 19:57:30 +0200

    extend TestCase initialisation of TestCompoundMap

--- tests/test_compound_map.py
+++ tests/test_compound_map.py
@@ -3,6 +3,12 @@ import pathlib
 import numpy as np
 from gmapy.gma_database_class import GMADatabase
 from gmapy.mappings.compound_map import CompoundMap
+from gmapy.data_management.database_IO import read_gma_database
+from gmapy.data_management.tablefuns import (
+    create_prior_table,
+    create_experiment_table
+)
+from gmapy.mappings.priortools import attach_shape_prior
 
 
 class TestCompoundMap(unittest.TestCase):
@@ -12,7 +18,15 @@ class TestCompoundMap(unittest.TestCase):
         dbpath = (pathlib.Path(__file__).parent / 'testdata' /
                 'data-2017-07-26.gma').resolve().as_posix()
         cls._dbpath = dbpath
-        cls._gmadb = GMADatabase(dbpath)
+        rawdb = read_gma_database(dbpath)
+        cls._priortable = create_prior_table(rawdb['prior_list'])
+        cls._exptable = create_experiment_table(rawdb['datablock_list'])
+        cls._priortable = attach_shape_prior((cls._priortable, cls._exptable))
+        cls._gmadb = GMADatabase(
+            prior_list=rawdb['prior_list'],
+            datablock_list=rawdb['datablock_list'],
+            remove_dummy=False
+        )
 
     def test_propagate_with_reduce_option(self):
         dt = self._gmadb.get_datatable()
