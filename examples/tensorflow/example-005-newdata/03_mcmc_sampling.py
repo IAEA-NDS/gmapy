7b8d3c439e535596e7c1752b1efe77a989a519b6
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-10-08 23:42:28 +0200

    add two tf mcmc examples: std2017 and newdata

---
+++ examples/tensorflow/example-005-newdata/03_mcmc_sampling.py
@@ -0,0 +1,42 @@
+import sys
+sys.path.append('../../..')
+import time
+import gc
+import tensorflow as tf
+import tensorflow_probability as tfp
+from gmapy.data_management.object_utils import (
+    load_objects, save_objects
+)
+from gmapy.tf_uq.inference import generate_MCMC_chain
+# NOTE: the numpy import is necessary because
+#       a method in the post instance loaded via dill
+#       depends on it
+import numpy as np
+tfb = tfp.bijectors
+
+
+post, likelihood = load_objects(
+    'output/01_model_preparation_output.pkl',
+    'post', 'likelihood'
+)
+optres, = load_objects(
+    'output/02_parameter_optimization_output.pkl',
+    'optres'
+)
+
+# define essential input quantities for MCMC
+optvals = optres.position
+
+s1 = time.time()
+chain, tracing_info = generate_MCMC_chain(
+    optvals, post.log_prob, post.neg_log_prob_hessian,
+    nugget=1e-8, step_size=1., num_burnin_steps=int(2e3),
+    num_results=int(20e3), num_leapfrog_steps=5
+)
+s2 = time.time()
+print(f's2-s1: {s2-s1}')
+
+save_objects(
+    'output/03_mcmc_sampling_output.pkl', locals(),
+    'chain', 'tracing_info'
+)
---
+++ examples/tensorflow/example-005-std2017/01_model_preparation.py
@@ -0,0 +1,117 @@
+import sys
+sys.path.append('../../..')
+import pandas as pd
+from scipy.sparse import block_diag
+import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+from gmapy.data_management.object_utils import (
+    save_objects
+)
+from gmapy.data_management.uncfuns import (
+    create_experimental_covmat,
+    create_datablock_covmat_list,
+    create_prior_covmat
+)
+from gmapy.mappings.tf.compound_map_tf \
+    import CompoundMap as CompoundMapTF
+from gmapy.data_management.database_IO import read_gma_database
+from gmapy.data_management.tablefuns import (
+    create_prior_table,
+    create_experiment_table,
+)
+from gmapy.mappings.priortools import (
+    attach_shape_prior,
+    initialize_shape_prior,
+    remove_dummy_datasets
+)
+from gmapy.mappings.tf.restricted_map import RestrictedMap
+from gmapy.mappings.tf.energy_dependent_absolute_usu_map_tf import (
+    EnergyDependentAbsoluteUSUMap,
+    create_endep_abs_usu_df
+)
+from gmapy.tf_uq.custom_distributions import (
+    MultivariateNormal,
+    MultivariateNormalLikelihood,
+    DistributionForParameterSubset,
+    UnnormalizedDistributionProduct
+)
+
+tfd = tfp.distributions
+tfb = tfp.bijectors
+
+# retrieve prior estimates and covariances from the database
+db_path = '../../../legacy-tests/test_002/input/data.gma'
+db = read_gma_database(db_path)
+remove_dummy_datasets(db['datablock_list'])
+
+priortable = create_prior_table(db['prior_list'])
+priorcov = create_prior_covmat(db['prior_list'])
+
+# prepare experimental quantities
+exptable = create_experiment_table(db['datablock_list'])
+expcov = create_experimental_covmat(db['datablock_list'])
+exptable['UNC'] = np.sqrt(expcov.diagonal())
+
+# For testing: perturb one dataset and see if we get a change
+# exptable.loc[exptable.NODE == 'exp_1025', 'DATA'] *= 1.5
+
+# initialize the normalization errors
+priortable, priorcov = attach_shape_prior((priortable, exptable), covmat=priorcov, raise_if_exists=False)
+compmap = CompoundMapTF((priortable, exptable), reduce=True)
+initialize_shape_prior((priortable, exptable), compmap)
+
+# some convenient shortcuts
+priorvals = priortable.PRIOR.to_numpy()
+expvals = exptable.DATA.to_numpy()
+
+# speed up the pdf log_prob calculations exploiting the block diagonal structure
+expcov_list, idcs_tuples = create_datablock_covmat_list(db['datablock_list'], relative=True)
+expchol_list = [tf.linalg.cholesky(x.toarray()) for x in expcov_list]
+expchol_op_list = [tf.linalg.LinearOperatorLowerTriangular(
+        x, is_non_singular=True, is_square=True
+    ) for x in expchol_list]
+
+# generate a restricted mapping blending out the fixed parameters
+is_adj = priorcov.diagonal() != 0.
+adj_idcs = np.where(is_adj)[0]
+fixed_pars = priorvals[~is_adj]
+fixed_idcs = np.where(~is_adj)[0]
+restrimap = RestrictedMap(
+    len(priorvals), compmap.propagate, compmap.jacobian,
+    fixed_params=fixed_pars, fixed_params_idcs=fixed_idcs
+)
+propfun = tf.function(restrimap.propagate)
+jacfun = tf.function(restrimap.jacobian)
+
+# generate the experimental covariance matrix
+expcov_chol = tf.linalg.LinearOperatorBlockDiag(
+    expchol_op_list, is_non_singular=True, is_square=True)
+expcov_linop = tf.linalg.LinearOperatorComposition(
+    [expcov_chol, expcov_chol.adjoint()],
+    is_self_adjoint=True, is_positive_definite=True
+)
+
+# generate the prior distribution
+is_adj_constr = is_adj & np.isfinite(priorcov.diagonal())
+is_adj_constr_idcs = np.where(is_adj_constr)[0]
+priorcov_chol = tf.linalg.LinearOperatorDiag(np.sqrt(priorcov.diagonal()[is_adj_constr]))
+prior_red = MultivariateNormal(priorvals[is_adj_constr], priorcov_chol)
+prior_red.log_prob_hessian(priorvals[is_adj_constr])
+prior = DistributionForParameterSubset(
+    prior_red, len(adj_idcs), is_adj_constr_idcs
+)
+
+# generate the likelihood
+likelihood = MultivariateNormalLikelihood(
+    len(adj_idcs), propfun, jacfun, expvals, expcov_chol,
+    approximate_hessian=True, relative=True
+)
+
+# combine prior and likelihood into posterior
+post = UnnormalizedDistributionProduct([prior, likelihood])
+
+
+save_objects('output/01_model_preparation_output.pkl', locals(),
+             'post', 'likelihood', 'priorvals', 'is_adj',
+             'priortable', 'exptable', 'expcov', 'expcov_linop')
