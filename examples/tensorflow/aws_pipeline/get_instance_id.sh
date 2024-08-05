e61c3027b66a4516faf3c649c9e53e5cd2f44a97
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-08-05 14:34:59 +0200

    remove examples/ folder from gmapy repo
    The examples will be stored in their own
    repository named `gmapy-examples` and probably
    included as a submodule in this repository.

--- examples/tensorflow/aws_pipeline/get_instance_id.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-instance_info_file="$1"
-if [ -z "$instance_info_file" ]; then
-    echo Please provide instance info file. Aborting. >&2
-    exit 1
-fi
-instance_id=$(cat "$instance_info_file" | jq -r .Instances[0].InstanceId)
-echo $instance_id
--- examples/tensorflow/aws_pipeline/get_instance_ip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-instance_info_file="$1"
-
-instance_id=$(cat "$instance_info_file" | jq -r .Instances[0].InstanceId)
-public_ip=$(aws ec2 describe-instances --instance-ids $instance_id \
-            --query 'Reservations[0].Instances[0].PublicIpAddress' \
-            --output text)
-echo $public_ip
