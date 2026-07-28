d877a01f978fd38218414bb1eb4d8f95101e2534
gschnabel <40870991+gschnabel@users.noreply.github.com>
2026-07-28 14:06:38 +0200

    support point-wise fission spectrum in fission average and SACS ratio mappings

--- gmapy/mappings/helperfuns/__init__.py
+++ gmapy/mappings/helperfuns/__init__.py
@@ -1,6 +1,7 @@
 from .helperfuns import (
     mapclass_with_params,
-    get_legacy_to_pointwise_fis_factors
+    get_legacy_to_pointwise_fis_factors,
+    get_fission_spectrum_interp
 )
 from .numeric_jacobian import numeric_jacobian
 from .romberg_integration import compute_romberg_integral
